de73e5a88d9df957897b366f2fabf75e65096faa
the-Astra <djs8743@rit.edu>
2026-07-29 13:35:34 -0400

    dev objects

---
+++ content/astra-missingno/dev_obj.lua
@@ -0,0 +1,12 @@
+PotatoPatchUtils.Developer({
+	name = 'theAstra',
+	colour = G.C.PURPLE,
+	fac_partner = 'MissingNumber'
+})
+
+PotatoPatchUtils.Developer({
+	name = 'MissingNumber',
+	colour = G.C.ORANGE,
+	fac_partner = 'theAstra'
+})
+
