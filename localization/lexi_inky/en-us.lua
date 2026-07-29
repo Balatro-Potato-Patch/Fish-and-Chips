955c855e402926f68c3010dcd6cf596f6a8ecedc
Lexi <hiimlexilol@gmail.com>
2026-07-29 15:40:45 -0600

    Inital stuff and square fish

---
+++ localization/lexi_inky/en-us.lua
@@ -0,0 +1,42 @@
+return {
+	descriptions = {
+		PotatoPatch = {
+			PotatoPatchDev_lexi = {
+				name = "lexi",
+				text = {
+					"hi",
+				},
+			},
+			PotatoPatchDev_inky = {
+				name = "inky",
+				text = {
+					"inky",
+				},
+			},
+		},
+		fac_Fish = {
+			fish_fac_l_i_square = {
+				name = "Square Fish",
+				text = {
+					"Played {C:attention}4s{} give",
+					"{C:fac_sand_dollars,f:fac_sand_dollars}${C:fac_sand_dollars}#1#{} when scored",
+				},
+				flavour = {
+					"i killed 4 circles",
+				},
+			},
+			fish_fac_l_i_smartass = {
+				name = "Smartass Fish",
+				text = {
+					"hi",
+					--[["Fish will sometimes fly across the screen",
+					"This fish gains {C:mult}+#2#{} when you click a flying fish",
+					"{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)",]]
+				},
+				flavour = {
+					"{element:1}",
+				},
+			},
+		},
+	},
+}
