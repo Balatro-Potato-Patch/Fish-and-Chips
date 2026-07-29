89fc742a1df583b041e3dad0202ded90b1410966
AllUniversal <alluniversal@gmx.ch>
2026-07-30 00:27:12 +0200

    Added boilerplate + `fishee.png`!!
    *Title, big steps

---
+++ content/aure-allu/Aure-Allu.lua
@@ -0,0 +1,28 @@
+PotatoPatchUtils.Developer({
+	name = 'Aure',
+	atlas = 'aure-allu_cards',
+	colour = G.C.ORANGE,
+	fac_partner = 'AllUniversal'
+})
+
+PotatoPatchUtils.Developer({
+	name = 'Alluniversal',
+	atlas = 'aure-allu_cards',
+	pos = {x = 1, y = 0},
+	colour = G.C.GREY,
+	fac_partner = 'Aure'
+})
+
+SMODS.Atlas({
+	key = "aure-allu_cards",
+	path = "core/cards.png",
+	px = 71,
+	py = 95,
+})
+
+SMODS.Atlas({
+	key = "aure-allu_fish",
+	path = "aure-allu/fishee.png",
+	px = 71,
+	py = 95,
+})
