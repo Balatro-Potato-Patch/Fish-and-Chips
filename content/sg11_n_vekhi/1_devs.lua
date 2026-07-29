3cf425a4e63dcf1a191f2f227cf6d3b67242c08d
SleepyG11 <makatymba2001@yandex.ru>
2026-07-30 01:53:11 +0300

    credits

---
+++ content/sg11_n_vekhi/1_devs.lua
@@ -0,0 +1,24 @@
+SMODS.Atlas({
+	key = "sg11_n_vekhi_credits",
+	path = "sg11_n_vekhi/credits.png",
+	px = 71,
+	py = 95,
+})
+
+PotatoPatchUtils.Developer({
+	name = "sleepyg11",
+	atlas = "fac_sg11_n_vekhi_credits",
+	pos = { x = 1, y = 0 },
+	colour = G.C.CHIPS,
+	fac_partner = "vevekhi",
+	loc = "fac_sleepyg11",
+})
+
+PotatoPatchUtils.Developer({
+	name = "vevekhi",
+	atlas = "fac_sg11_n_vekhi_credits",
+	pos = { x = 0, y = 0 },
+	colour = G.C.MULT,
+	fac_partner = "sleepyg11",
+	loc = "fac_vevekhi",
+})
---
+++ localization/sg11_n_vekhi/en-us.lua
@@ -0,0 +1,23 @@
+return {
+	descriptions = {
+		PotatoPatch = {
+			fac_sleepyg11 = {
+				name = "SleepyG11",
+				text = {
+					{
+						"Amazing f***shing",
+						"{C:inactive}To be added...{}",
+					},
+				},
+			},
+			fac_vevekhi = {
+				name = "vevekhi",
+				text = {
+					{
+						"{C:inactive}To be added...{}",
+					},
+				},
+			},
+		},
+	},
+}
