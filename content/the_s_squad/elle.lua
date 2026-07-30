1003b8ed998e8c48f1d6e39cde534747b3d46905
ellestuff. <ellef1566@gmail.com>
2026-07-30 14:41:40 +0100

    release the swarm

--- content/the_s_squad/elle.lua
+++ content/the_s_squad/elle.lua
@@ -1,4 +1,31 @@
 FishAndChips.Fish {
+	key = "tss_resident",
+	atlas = "tss_fish",
+	pos = { x = 0, y = 0 },
+	weight = 13,
+	ppu_coder = { "slimestuff" },
+	ppu_artist = { "slimestuff" },
+	attributes = { "mult" },
+	config = { extra = { mult = 0, mult_mod = 2 } },
+	environments = {
+		city_river = 1,
+		backroom = 4
+	},
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.mult_mod, card.ability.extra.mult } }
+	end,
+	calculate = function(self, card, context)
+		if context.end_of_round and context.main_eval and not context.blueprint then
+			card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_mod
+			
+			return { message = localize("k_upgrade_ex") }
+		end
+
+		if context.joker_main then return { mult = card.ability.extra.mult } end
+	end
+}
+
+FishAndChips.Fish {
 	key = "tss_chesh",
 	atlas = "tss_fish",
 	pos = { x = 0, y = 0 },
@@ -16,52 +43,7 @@ FishAndChips.Fish {
 		return { vars = { num, dem, card.ability.extra.xmult_mod, card.ability.extra.xmult } }
 	end,
 	calculate = function(self, card, context)
-		if context.fac_end_fishing and not context.failed and SMODS.pseudorandom_probability(card,"fcc_tss_chesh",1,card.ability.extra.odds) then
-			local f = G.FISHING.fac_fish_reward_area.cards[1]
-
-			f.tss_cheshed = true
-			
-			card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.xmult_mod
-			
-			f.states.click.can = false -- Apparently it should be like this already but they forgot. Will remove once that is patched :p
-			
-			G.E_MANAGER:add_event(Event({func=function()
-				card.area:remove_card(card)
-				G.FISHING.fac_fish_reward_area:emplace(card)
-				play_sound("whoosh")
-				card.T.w = card.T.w*1.3
-				card.T.h = card.T.h*1.3
-			return true end}))
-			
-			G.E_MANAGER:add_event(Event({trigger = "after", delay = .65, func=function()
-				card:juice_up()
-				SMODS.calculate_effect({message = localize("fac_tss_chesh_giggle_"..pseudorandom("fac_tss_chesh_giggle",1,4)), colour = G.C.PURPLE, instant = true}, card)
-			return true end}))
-
-			for i = 1, 5, 1 do
-				G.E_MANAGER:add_event(Event({trigger = "after", delay = .65, func=function()
-					card:juice_up()
-					f:juice_up()
-					play_sound("fac_tss_eat"..pseudorandom("fac_tss_chesh_eat_sfx",1,3))
-				return true end}))
-			end
-
-			G.E_MANAGER:add_event(Event({trigger = "after", delay = .65, func=function()
-				f:juice_up()
-				SMODS.destroy_cards(f)
-				f:start_dissolve({HEX("917bad")})
-			return true end}))
-
-			G.E_MANAGER:add_event(Event({func=function()
-				card.area:remove_card(card)
-				G.fac_fish_area:emplace(card)
-				play_sound("fac_tss_burp")
-				card.T.w = card.T.w/1.3
-				card.T.h = card.T.h/1.3
-			return true end}))
-
-		end
-
+		-- Chesh eating handled in dev calculate so they can all flock at once like hungry pirahnas
 		if context.joker_main and card.ability.extra.xmult ~= 1 then return { xmult = card.ability.extra.xmult } end
 	end
 }
@@ -71,33 +53,6 @@ function CardArea:emplace(card)
 	if card.tss_cheshed then return end
 	emplace_hook(self,card)
 end
-
-FishAndChips.Fish {
-	key = "tss_resident",
-	atlas = "tss_fish",
-	pos = { x = 0, y = 0 },
-	weight = 13,
-	ppu_coder = { "slimestuff" },
-	ppu_artist = { "slimestuff" },
-	attributes = { "mult" },
-	config = { extra = { mult = 0, mult_mod = 2 } },
-	environments = {
-		city_river = 1,
-		backroom = 4
-	},
-	loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.mult_mod, card.ability.extra.mult } }
-	end,
-	calculate = function(self, card, context)
-		if context.end_of_round and not context.blueprint then
-			card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_mod
-			
-			return { message = localize("k_upgrade_ex") }
-		end
-
-		if context.joker_main then return { mult = card.ability.extra.mult } end
-	end
-}
 
 FishAndChips.Fish {
 	key = "tss_caviar",
--- content/the_s_squad/the_s_squad.lua
+++ content/the_s_squad/the_s_squad.lua
@@ -3,7 +3,73 @@ PotatoPatchUtils.Developer{
 	atlas = 'fac_cards',
 	colour = HEX("FF53A9"),
 	fac_partner = 'azazel',
-	loc = true
+	loc = true,
+	calculate = function(self, context)
+		-- Putting Chesh here so they can all swarm at once like a pack of hungry piranhas
+		if context.fac_end_fishing and not context.failed then
+			local f = G.FISHING.fac_fish_reward_area.cards[1]
+			local cheshlist = {}
+			for i, card in ipairs(SMODS.find_card("fish_fac_tss_chesh")) do
+				if SMODS.pseudorandom_probability(card,"fcc_tss_chesh",1,card.ability.extra.odds) then
+					cheshlist[#cheshlist+1] = card
+				end
+			end
+			
+			if #cheshlist < 1 then return end
+			
+			f.tss_cheshed = true
+			f.states.click.can = false -- Apparently it should be like this already but they forgot. Will remove once that is patched :p
+			
+			
+			-- Move Cheshes to the fish
+			for _, v in ipairs(cheshlist) do
+				G.E_MANAGER:add_event(Event({trigger = "after", delay = .8/#cheshlist, func=function()
+					v.area:remove_card(v)
+					G.FISHING.fac_fish_reward_area:emplace(v)
+					play_sound("whoosh")
+					v.T.w = v.T.w*1.3
+					v.T.h = v.T.h*1.3
+				return true end}))
+			end
+			
+			for _, v in ipairs(cheshlist) do
+				G.E_MANAGER:add_event(Event({trigger = "after", delay = .8/#cheshlist, func=function()
+					v:juice_up()
+					SMODS.calculate_effect({message = localize("fac_tss_chesh_giggle_"..pseudorandom("fac_tss_chesh_giggle",1,4)), colour = G.C.PURPLE, instant = true}, v)
+				return true end}))
+			end
+
+			for i = 1, 5 do
+				for _, v in ipairs(cheshlist) do
+					G.E_MANAGER:add_event(Event({trigger = "after", delay = .8/#cheshlist, func=function()
+						v:juice_up()
+						f:juice_up()
+						play_sound("fac_tss_eat"..pseudorandom("fac_tss_chesh_eat_sfx",1,3))
+					return true end}))
+				end
+			end
+
+			G.E_MANAGER:add_event(Event({trigger = "after", delay = .8, func=function()
+				f:juice_up()
+				SMODS.destroy_cards(f)
+				f:start_dissolve({HEX("917bad")})
+				for _, v in ipairs(cheshlist) do
+					v.ability.extra.xmult = v.ability.extra.xmult + v.ability.extra.xmult_mod
+				end
+			return true end}))
+
+			for _, v in ipairs(cheshlist) do
+				G.E_MANAGER:add_event(Event({trigger = "after", delay = .8/#cheshlist, func=function()
+					v.area:remove_card(v)
+					G.fac_fish_area:emplace(v)
+					play_sound("fac_tss_burp")
+					v.T.w = v.T.w/1.3
+					v.T.h = v.T.h/1.3
+				return true end}))
+			end
+
+		end
+	end
 }
 
 PotatoPatchUtils.Developer{
