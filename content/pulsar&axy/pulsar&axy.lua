41036993cc34a2de4326393a82c8d30bbe850772
Axyraandas <digatadefender495@gmail.com>
2026-07-30 14:00:38 -0400

    initializing our content.lua file

--- content/pulsar&axy/pulsar&axy.lua
+++ content/pulsar&axy/pulsar&axy.lua
@@ -1,1 +1,28 @@
-code
+--#region Fish
+
+FishAndChips.Fish {
+	key = "cod",
+	atlas = "fish",
+	pos = { x = 3, y = 0 },
+	weight = 10,
+	ppu_coder = { "Mack" },
+	ppu_artist = { "GhostSalt" },
+	attributes = { "chips" },
+	config = {
+		extra = {
+			chips = 30
+		}
+	},
+	environments = {
+		pier = 10,
+		city_river = 2.5
+	},
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.chips } }
+	end,
+	calculate = function(self, card, context)
+		if context.joker_main then return { chips = card.ability.extra.chips } end
+	end,
+}
+
+--#endregion
