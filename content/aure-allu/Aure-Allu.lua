50ba6e0cca766a9947e615d633fb221392cb9681
AllUniversal <alluniversal@gmx.ch>
2026-07-30 20:15:23 +0200

    Implemented Eel of Fortune, Moldfish, Shrimp and Mult Mola
    +/*Title

--- content/aure-allu/Aure-Allu.lua
+++ content/aure-allu/Aure-Allu.lua
@@ -1,13 +1,13 @@
 PotatoPatchUtils.Developer({
 	name = 'Aure',
-	atlas = 'aure-allu_cards',
+	atlas = 'fac_aure-allu_cards',
 	colour = G.C.ORANGE,
 	fac_partner = 'AllUniversal'
 })
 
 PotatoPatchUtils.Developer({
-	name = 'Alluniversal',
-	atlas = 'aure-allu_cards',
+	name = 'AllUniversal',
+	atlas = 'fac_aure-allu_cards',
 	pos = {x = 1, y = 0},
 	colour = G.C.GREY,
 	fac_partner = 'Aure'
@@ -26,6 +26,48 @@ SMODS.Atlas({
 	px = 71,
 	py = 95,
 })
+
+
+zero_signed = function (value, infix)
+	local v = value ~= 0 and SMODS.signed(value) or "+0"
+	return string.sub(v, 1, 1) .. (infix or "") .. string.sub(v, 2)
+end
+
+
+filter_list = function (t, exclude_map)
+	local out = {}
+	exclude_map = exclude_map or {}
+	for i, elem in ipairs(t) do
+		if not exclude_map[elem] then table.insert(out, elem) end
+	end
+	return out
+end
+
+function table_find(t, value)
+	if not type(t) == "table" then return end
+	for k, v in pairs(t) do
+		if v == value then return k end
+	end
+	return nil
+end
+
+function table_get_subfield(_table, key_string_or_keys)
+    if type(_table) ~= "table" then sendWarnMessage("table_get_subfield called with invalid table argument", "utils"); return end 
+    if type(key_string_or_keys) ~= "string" and type(key_string_or_keys) ~= "table" then sendWarnMessage(string.format("table_get_subfield called with invalid key_string '%s'.", key_string_or_keys), "utils"); return end
+    local _t = _table
+    if type(key_string_or_keys) == "string" then
+        for field in string.gmatch(key_string_or_keys, "[^.]+") do
+            _t = _t[field]
+            if not _t then return end
+        end
+    else 
+        for _, field in ipairs(key_string_or_keys) do
+            _t = _t[field]
+            if not _t then return end
+        end
+    end
+    return _t
+end
 
 
 --#region Fish
@@ -48,7 +90,7 @@ FishAndChips.Fish {
         }
 	},
 	environments = {
-		pond = 10,
+		calm_pond = 10,
 		pier = 10,
 		city_river = 10,
 		swamp = 10,
@@ -157,7 +199,7 @@ FishAndChips.Fish {
 		},
 	},
 	environments = {
-		pond = 10,
+		calm_pond = 10,
 		garden = 10,
 		aquifer = 7,
 		chocolate_river = 3,
@@ -171,6 +213,25 @@ FishAndChips.Fish {
 }
 
 -- Moldfish
+local function get_average_property(cards, property)
+	if #cards <= 0 then return 0 end
+	local total = 0
+	for i, card in ipairs(cards) do
+		total = total + (table_get_subfield(card, property) or 0)
+	end
+	return total / #cards
+end
+
+local ease_round_ref = ease_round
+function ease_round(...)
+	ease_round_ref(...)
+	for _, area in ipairs({G.jokers, G.consumeables, G.fac_fish_area}) do
+		for _, card in ipairs(area.cards) do
+			card.ability.rounds_held = card.ability.rounds_held and card.ability.rounds_held + 1 or 1
+		end
+	end
+end
+
 FishAndChips.Fish {
 	key = "moldfish",
 	atlas = "aure-allu_fish",
@@ -178,10 +239,10 @@ FishAndChips.Fish {
 	weight = 1,
 	ppu_coder = { "AllUniversal" },
 	ppu_artist = { "AllUniversal" },
-	attributes = {  },
+	attributes = { "mult" },
 	config = {
 		extra = {
-			
+			mult_per_average_round = 13
 		},
 	},
 	environments = {
@@ -189,13 +250,17 @@ FishAndChips.Fish {
 		city_river = 8,
 		aquifer = 6,
 		swamp = 6,
-		styx = 2,
 	},
 	loc_vars = function(self, info_queue, card)
-		return { vars = { } }
+		local average = G.fac_fish_area and get_average_property(G.fac_fish_area.cards, "ability.rounds_held") or 0
+		return { vars = { zero_signed(card.ability.extra.mult_per_average_round), zero_signed(math.floor(card.ability.extra.mult_per_average_round * average)) } }
 	end,
 	calculate = function(self, card, context)
-		
+		if context.joker_main then
+			return {
+                mult = math.floor(card.ability.extra.mult_per_average_round * get_average_property(G.fac_fish_area.cards, "ability.rounds_held"))
+            }
+		end
 	end,
 }
 
@@ -207,11 +272,16 @@ FishAndChips.Fish {
 	weight = 1,
 	ppu_coder = { "AllUniversal" },
 	ppu_artist = { "AllUniversal" },
-	attributes = {  },
+	attributes = { "mult" },
 	config = {
 		extra = {
-			
+			mult_gain = 4,
+			total_mult = 0,
 		},
+		immutable = {
+			last_slots = {},
+			last_slots_max = 2,
+		}
 	},
 	environments = {
 		city_river = 10,
@@ -221,10 +291,35 @@ FishAndChips.Fish {
 		wormhole = 9, 
 	},
 	loc_vars = function(self, info_queue, card)
-		return { vars = { } }
+		return { vars = { zero_signed(card.ability.extra.mult_gain), card.ability.immutable.last_slots_max, zero_signed(card.ability.extra.total_mult) } }
 	end,
 	calculate = function(self, card, context)
-		
+		if context.before and not context.blueprint then
+            local same_slot = false
+			for _, slot in ipairs(card.ability.immutable.last_slots) do
+				if slot == table_find(G.fac_fish_area.cards, card) then
+					same_slot = true; break
+				end
+			end
+			if #card.ability.immutable.last_slots >= card.ability.immutable.last_slots_max then table.remove(card.ability.immutable.last_slots, 1) end
+			table.insert(card.ability.immutable.last_slots, card.rank)
+            if same_slot then
+                local last_mult = card.ability.extra.total_mult
+                card.ability.extra.total_mult = 0
+                if last_mult > 0 then
+                    return {
+                        message = localize('k_reset')
+                    }
+                end
+            else
+                card.ability.extra.total_mult = card.ability.extra.total_mult + card.ability.extra.mult_gain
+            end
+        end
+        if context.joker_main then
+            return {
+                mult = card.ability.extra.total_mult
+            }
+        end
 	end,
 }
 
@@ -251,12 +346,12 @@ FishAndChips.Fish {
 		soup = 1,
 	},
 	loc_vars = function(self, info_queue, card)
-		local empty = G.fac_fish_area.card_limit - #G.fac_fish_area.cards
-		return { vars = { SMODS.signed(card.ability.extra.mult_per_slot), SMODS.signed(empty * card.ability.extra.mult_per_slot) } }
+		local empty = G.fac_fish_area and G.fac_fish_area.config.card_limits.base - #G.fac_fish_area.cards or 0
+		return { vars = { zero_signed(card.ability.extra.mult_per_slot), zero_signed(empty * card.ability.extra.mult_per_slot) } }
 	end,
 	calculate = function(self, card, context)
 		if context.joker_main then
-			local empty = G.fac_fish_area.card_limit - #G.fac_fish_area.cards
+			local empty = G.fac_fish_area.config.card_limits.base - #G.fac_fish_area.cards
 			return {
 				mult = empty * card.ability.extra.mult_per_slot
 			}
@@ -275,22 +370,66 @@ FishAndChips.Fish {
 	attributes = {  },
 	config = {
 		extra = {
-			
+			eel_odds = 4
 		},
 	},
 	environments = {
-		pond = 3,
 		pier = 5,
 		volcano = 10,
 		aquifer = 8,
-		styx = 1,
+		swamp = 8,
 	},
 	loc_vars = function(self, info_queue, card)
-		return { vars = {  } }
+		info_queue[#info_queue+1] = G.P_CENTERS.e_foil
+		info_queue[#info_queue+1] = G.P_CENTERS.e_holo
+		info_queue[#info_queue+1] = G.P_CENTERS.e_polychrome
+		local numerator_eel, denominator_eel = SMODS.get_probability_vars(card, 1, card.ability.extra.eel_odds, "fac_aure-allu_eel_of_fortune")
+		return { vars = { numerator_eel, denominator_eel } }
 	end,
-	calculate = function(self, card, context)
-		
+	use = function (self, card)
+		-- Thanks https://github.com/nh6574/VanillaRemade/blob/main/src/tarots.lua Wheel of Fortune
+		if SMODS.pseudorandom_probability(card, 'fac_aure-allu_eel_of_fortune', 1, card.ability.extra.eel_odds) then
+            local editionless_fishee = SMODS.Edition:get_edition_cards({cards = filter_list(G.fac_fish_area.cards, {[card] = true})}, true)
+            local eligible_card = pseudorandom_element(editionless_fishee, 'fac_aure-allu_eel_of_fortune')
+            local edition = SMODS.poll_edition { key = "fac_aure-allu_eel_of_fortune", guaranteed = true, no_negative = true, options = { 'e_polychrome', 'e_holo', 'e_foil' } }
+            eligible_card:set_edition(edition, true)
+            check_for_unlock({ type = 'have_edition' })
+        else
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.4,
+                func = function()
+                    attention_text({
+                        text = localize('k_nope_ex'),
+                        scale = 1.3,
+                        hold = 1.4,
+                        major = card,
+                        backdrop_colour = FishAndChips.C.FAC_PRIMARY,
+                        align = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK or G.STATE == G.STATES.SMODS_BOOSTER_OPENED) and
+                            'tm' or 'cm',
+                        offset = { x = 0, y = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK or G.STATE == G.STATES.SMODS_BOOSTER_OPENED) and -0.2 or 0 },
+                        silent = true
+                    })
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        delay = 0.06 * G.SETTINGS.GAMESPEED,
+                        blockable = false,
+                        blocking = false,
+                        func = function()
+                            play_sound('tarot2', 0.76, 0.4)
+                            return true
+                        end
+                    }))
+                    play_sound('tarot2', 1, 0.4)
+                    card:juice_up(0.3, 0.5)
+                    return true
+                end
+            }))
+        end
 	end,
+	can_use = function (self, card)
+		return next(SMODS.Edition:get_edition_cards({cards = filter_list(G.fac_fish_area.cards, {[card] = true})}, true))
+	end
 }
 
 -- #endregion
--- localization/aure-allu/en-us.lua
+++ localization/aure-allu/en-us.lua
@@ -2,13 +2,12 @@ return {
     descriptions = {
         fac_Fish = {
             fish_fac_the_original___starfish = {
-                name = {"The Original    ", "   Starfish"},
+                name = {"The Original    ", "     Starfish"},
                 text = {
                     ""
                 },
                 flavor = {
-                    "These fish are",
-                    "pissing me off"
+                    "These fish are pissing me off",
                 }
             },
             fish_fac_cheap_cheep = {
@@ -41,26 +40,34 @@ return {
             fish_fac_moldfish = {
                 name = {"Moldfish"},
                 text = {
-                    ""
+                    "Blubs {C:mult}#1#{} Mult times the",
+                    "{C:attention}average{} number of {C:attention}rounds{}",
+                    "caught {C:attention}Fish{} have been held",
+                    "{C:inactive}(Currently {C:mult}#2#{C:inactive} Mult)"
                 },
                 flavor = {
-                    ""
+                    "I'm mold!"
                 }
             },
             fish_fac_shrimp = {
                 name = {"Shrimp"},
                 text = {
-                    ""
+                    "Gains {C:mult}#1#{} Mult if",
+                    "this {C:attention}Fish{} resides in",
+                    "a different {C:attention}slot{} from",
+                    "last {C:attention}#2#{} hands {C:attention}played{}",
+                    "{C:inactive,s:0.8}(Resets otherwise)",
+                    "{C:inactive}(Currently {C:mult}#3#{C:inactive} Mult)"
                 },
                 flavor = {
-                    ""
+                    "It's as shrimple as that!"
                 }
             },
             fish_fac_mult_mola = {
                 name = {"Mult Mola"},
                 text = {
                     "Blubs {C:mult}#1#{} Mult for",
-                    "each empty {C:chips}Fish {C:attention}slot",
+                    "each empty {C:attention}Bucket slot",
                     "{C:inactive}(Currently {C:mult}#2#{C:inactive} Mult)"
                 },
                 flavor = {
@@ -70,10 +77,12 @@ return {
             fish_fac_eel_of_fortune = {
                 name = {"Eel of Fortune"},
                 text = {
-                    ""
+                    "Use for {C:green}#1# in #2#{} chance",
+                    "to add {C:dark_edition}Foil{}, {C:dark_edition}Holographic{}, or",
+                    "{C:dark_edition}Polychrome{} to a random {C:attention}Fish"
                 },
                 flavor = {
-                    ""
+                    "Big money no swamp-eels"
                 }
             },
         },
