eec79e78920ef51726226124ea44d89dd8ede626
Abel-sketch <155697463+Abel-sketch@users.noreply.github.com>
2026-07-30 00:53:51 -0600

    localizacion

--- localization/Sepa/en-us.lua
+++ localization/Sepa/en-us.lua
@@ -4,15 +4,35 @@ return {
             fish_fac_clownfish = {
                 name = "Clownfish",
                 text = {
-                    "{C:mult}+#1#{} Mult when",
-                    "played hand is",
-                    "the {C:attention}first{} played",
-                    "in the round"
+                    "On {C:attention}first played hand{}",
+                    "in round, {C:mult}+#1#{} Mult"
                 },
                 flavor = {
                     "Quite the famous fish",
                     "due to the documentary",
                     "of one his species lost son"
+                }
+            },
+            fish_fac_bombfish = {
+                name = "{f:fac_ultra}Bomb Fish{}",
+                text = {
+                    {
+                        "Creates {C:attention}2{} two {C:tarot}tarot{} cards when",
+                        "defused. If {C:attention}attempt{} count hits 0,",
+                        "sets money to {C:gold}$0{}. Play {C:attention}#4#{}",
+                        "to advance the defuse count {C:green}[#1#/#2#]{}"
+                    },
+                    {
+                        "{C:attention}Attempts{} go down for every round",
+                        "that the disable count {C:attention}doesn't go up.{}",
+                        "{C:green}Current attempts:{} #3#"
+                    }
+                },
+                flavor = {
+                    "{f:fac_ultra}Often found on flowing water.{}",
+                    "{f:fac_ultra}Has a self defence mechanism{}",
+                    "{f:fac_ultra}that causes bodly harm{}",
+                    "{f:fac_ultra}to unaware fishers.{}"
                 }
             },
 
