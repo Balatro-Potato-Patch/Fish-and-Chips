cf73cdc9a9f61ad55312e6d6e2091f3f19833d62
hayaunderscore <stophayapls123@proton.me>
2026-07-31 12:21:27 +0800

    refactor: move fishes to their own folders

---
+++ content/hayayaya/ellen/8f.lua
@@ -0,0 +1,49 @@
+FishAndChips.Fish({
+	key = "8f", -- 3 NEW
+	weight = 3,
+	cost = -3,
+	environments = {
+		calm_pond = 1,
+	},
+	ppu_coder = { "Ellen (Haya)" },
+	ppu_artist = { "Pepix" },
+	attributes = {
+		"usable",
+	},
+	decision_min = math.huge,
+	decision_max = math.huge,
+	vel_limit = 0,
+	config = { immutable = { count = 0, max = 8 } },
+	loc_vars = function(self, info_queue, card)
+		return {
+			vars = { card.ability.immutable.count, card.ability.immutable.max },
+			key = self.key .. (card.ability.immutable.count >= card.ability.immutable.max and "_alt" or ""),
+		}
+	end,
+	can_use = function(self, card)
+		return G.STATE == G.STATES.BLIND_SELECT and card.ability.immutable.count >= card.ability.immutable.max
+	end,
+	use = function(self, card)
+		-- TODO
+	end,
+	calculate = function(self, card, context)
+		if context.end_of_round and context.main_eval and card.ability.immutable.count < card.ability.immutable.max then
+			card.ability.immutable.count = card.ability.immutable.count + 1
+			return {
+				message = string.format("%i/%i", card.ability.immutable.count, card.ability.immutable.max),
+			}
+		end
+	end,
+	on_catch = function(self, card)
+		HayayayaUtils.stop_music()
+		G.E_MANAGER:add_event(Event({
+			trigger = "after",
+			delay = 0.5 * G.SPEEDFACTOR,
+			func = function()
+				play_sound("fac_hayayaya_mistake")
+				return true
+			end,
+		}))
+		delay(2 * G.SPEEDFACTOR)
+	end,
+})
---
+++ content/hayayaya/pepix/codamite.lua
@@ -0,0 +1,80 @@
+FishAndChips.Fish({
+	key = "codamite",
+	weight = 10,
+	environments = {
+		pier = 0.2,
+		backroom = 1,
+	},
+	ppu_coder = { "Ellen (Haya)" },
+	ppu_artist = { "Pepix" },
+	attributes = {
+		"boss_blind",
+		"usable",
+	},
+	atlas = "hayayaya_fih",
+	pos = { x = 1, y = 0 },
+	can_use = function(self, card)
+		return G.STATE == G.STATES.SELECTING_HAND
+	end,
+	use = function(self, card)
+		delay(0.5)
+
+		card:highlight(false)
+
+		-- If this is bigger than 0.5 then fuck you
+		local gambling = pseudorandom("hayayaya_explosion_chance_" .. G.GAME.round_resets.ante, 0, 1) > 0.5
+
+		G.E_MANAGER:add_event(Event({
+			func = function()
+				card.children.hayayaya_explosion = SMODS.create_sprite(
+					card.T.x,
+					card.T.y,
+					card.T.w * 2.5,
+					card.T.h * 2.65,
+					"fac_hayayaya_explosion",
+					{ x = 0, y = 0 }
+				)
+				card.children.hayayaya_explosion.role.role_type = "Minor"
+				card.children.hayayaya_explosion.role.major = card.children.center
+				card.children.hayayaya_explosion.role.offset =
+					{ x = -card.children.hayayaya_explosion.T.w / 4, y = -card.children.hayayaya_explosion.T.h / 4 }
+				card.children.hayayaya_explosion.hayayaya_explosion = true
+				SMODS.mod_blind_size({ mult = gambling and 2 or 0.5, card = G.GAME.blind, effect = {} })
+				play_sound("fac_hayayaya_explosion")
+				return true
+			end,
+		}))
+
+		G.E_MANAGER:add_event(Event({
+			trigger = "after",
+			delay = 0.2,
+			func = function()
+				card.children.center.states.visible = false
+				return true
+			end,
+		}))
+
+		G.E_MANAGER:add_event(Event({
+			trigger = "after",
+			delay = 0.5,
+			func = function()
+				-- I'm sure this won't have consequences!
+				card:remove()
+				return true
+			end,
+		}))
+
+		-- delay(0.5)
+	end,
+})
+
+local animate = AnimatedSprite.animate
+---@diagnostic disable-next-line
+function AnimatedSprite:animate()
+	local frame_finished = (math.floor((G.TIMERS.REAL - self.offset_seconds) / self.current_animation.frame_duration))
+		> 0
+	animate(self)
+	if frame_finished and self.current_animation.current >= 17 and self.hayayaya_explosion then
+		self:remove()
+	end
+end
