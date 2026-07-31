731b548b3df634f95573d51ddbad8e343eb02226
pickzel <93448366+Pixeldav@users.noreply.github.com>
2026-07-30 21:16:44 -0500

    others

--- content/pnf/pizzatower.lua
+++ content/pnf/pizzatower.lua
@@ -1,189 +1,271 @@
-
+PotatoPatchUtils.Developer({
+    name = 'FirstTry',
+    atlas = 'fac_pnf_blueax',
+    loc = true,
+    colour = G.C.SECONDARY_SET.Spectral,
+    display_size = { w = 71 * 20.7, h = 95 * 20.7 },
+    fac_partner =
+    'Pixel',            -- Only use this if you have a partner! This should be a string that's the same as your partner's PPU.Dev name property
+})
 
 PotatoPatchUtils.Developer({
-	name = 'FirstTry',
-	atlas = 'fac_pnf_blueax',
-	colour = G.C.SECONDARY_SET.Spectral,
-	fac_partner = 'Pixel' -- Only use this if you have a partner! This should be a string that's the same as your partner's PPU.Dev name property
-})
-
-PotatoPatchUtils.Developer({
-	name = 'Pixel',
-	atlas = 'fac_pnf_pixelfish',
-	colour = G.C.SECONDARY_SET.Planet,
-	fac_partner = 'FirstTry' -- Only use this if you have a partner! This should be a string that's the same as your partner's PPU.Dev name property
-})
-
-SMODS.Atlas({
-	key = "pnf_blueax", -- Please include your name/team name in your atlas keys
-	path = "pnf/SuspiciousBlueAxolotl.png",
-	px = 71,
-	py = 95,
-})
-
-SMODS.Atlas({
-	key = "pnf_dupli", -- Please include your name/team name in your atlas keys
-	path = "pnf/Barramunduplicare.png",
-	px = 71,
-	py = 95,
+    name = 'Pixel',
+    atlas = 'fac_pnf_pixelcredits',
+    loc = true,
+    colour = G.C.SECONDARY_SET.Planet,
+    fac_partner =
+    'FirstTry',            -- Only use this if you have a partner! This should be a string that's the same as your partner's PPU.Dev name property
+    click = function(self)
+        play_sound("fac_pnf_pixelsounds")
+        G.E_MANAGER:add_event(Event({
+            trigger = 'before',
+            func = function()
+                self:juice_up(1, 0.2)
+                return true
+            end
+        }))
+    end
 })
 
 SMODS.Atlas({
-	key = "pnf_pixelfish", -- Please include your name/team name in your atlas keys
-	path = "pnf/PixelFish.png",
-	px = 71,
-	py = 95,
+    key = "pnf_blueax", -- Please include your name/team name in your atlas keys
+    path = "pnf/SuspiciousBlueAxolotl.png",
+    px = 71,
+    py = 95,
+})
+
+SMODS.Atlas({
+    key = "pnf_dupli", -- Please include your name/team name in your atlas keys
+    path = "pnf/Barramunduplicare.png",
+    px = 71,
+    py = 95,
+})
+
+SMODS.Atlas({
+    key = "pnf_pixelfish", -- Please include your name/team name in your atlas keys
+    path = "pnf/PixelFish.png",
+    px = 71,
+    py = 95,
+})
+
+SMODS.Atlas({
+    key = "pnf_pixelcredits", -- Please include your name/team name in your atlas keys
+    path = "pnf/PixelCredits.png",
+    px = 71,
+    py = 95,
+})
+
+SMODS.Sound({
+    key = "pnf_pixelsounds", -- Please include your name/team name in your atlas keys
+    path = "pnf/pixelnoise.ogg",
 })
 
 FishAndChips.Fish {
-	key = "blueax",
-	atlas = "pnf_blueax",
-	pos = { x = 0, y = 0 },
-	weight = 1,
+    key = "blueax",
+    atlas = "pnf_blueax",
+    pos = { x = 0, y = 0 },
+    weight = 1,
     blueprint_compat = true,
-	ppu_coder = { "FirstTry" },
-	ppu_artist = { "FirstTry" },
-	attributes = { "mult", "chips", "xmult", "economy" },
-	config = {
-		extra = {
-			scoring = 2,
+    ppu_coder = { "FirstTry" },
+    ppu_artist = { "FirstTry" },
+    attributes = { "mult", "chips", "xmult", "economy" },
+    config = {
+        extra = {
+            scoring = 2,
             gain = 2,
             trigger = false
-		},
+        },
         immutable = {
             revert = 2
         }
-	},
-	environments = {
+    },
+    environments = {
         wormhole = 1,
         backroom = 1
     },
-	loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.scoring, card.ability.extra.gain, colours = { HEX("4db1f6") } } }
-	end,
-	    calculate = function(self, card, context)
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.scoring, card.ability.extra.gain, colours = { HEX("4db1f6") } } }
+    end,
+    calculate = function(self, card, context)
         local eval = function(card) return card.ability.extra.trigger == true end
         juice_card_until(card, eval, false)
         if context.joker_main then
             if card.ability.extra.trigger then
                 local ret = {}
                 local scoreret = pseudorandom(pseudoseed("fish_fac_blueax"), 1, 10)
-            if scoreret == 1 or scoreret == 10 then
-                        ret.chips = card.ability.extra.scoring
-            end
-            if scoreret == 2 or scoreret == 10 then
-                        ret.mult = card.ability.extra.scoring
-            end
-            if scoreret == 3 or scoreret == 10 then
-                        ret.xmult = (card.ability.extra.scoring/2)
-            end
-            if scoreret == 4 or scoreret == 10 then
-                        ret.xchips = (card.ability.extra.scoring/2)
-            end
-            if scoreret == 5 or scoreret == 10 then
-                        ret.score = card.ability.extra.scoring
-            end
-            if scoreret == 6 or scoreret == 10 then
-                        ret.xscore = (card.ability.extra.scoring/2)
-            end
-            if scoreret == 7 or scoreret == 10 then
-                        ret.blindsize = -card.ability.extra.scoring
-            end
-            if scoreret == 8 or scoreret == 10 then
-                        ret.xblindsize = -(card.ability.extra.scoring/2)
-            end
-            if scoreret == 9 or scoreret == 10 then
-                        ret.dollars = card.ability.extra.scoring
-            end
+                if scoreret == 1 or scoreret == 10 then
+                    ret.chips = card.ability.extra.scoring
+                end
+                if scoreret == 2 or scoreret == 10 then
+                    ret.mult = card.ability.extra.scoring
+                end
+                if scoreret == 3 or scoreret == 10 then
+                    ret.xmult = (card.ability.extra.scoring / 2)
+                end
+                if scoreret == 4 or scoreret == 10 then
+                    ret.xchips = (card.ability.extra.scoring / 2)
+                end
+                if scoreret == 5 or scoreret == 10 then
+                    ret.score = card.ability.extra.scoring
+                end
+                if scoreret == 6 or scoreret == 10 then
+                    ret.xscore = (card.ability.extra.scoring / 2)
+                end
+                if scoreret == 7 or scoreret == 10 then
+                    ret.blindsize = -card.ability.extra.scoring
+                end
+                if scoreret == 8 or scoreret == 10 then
+                    ret.xblindsize = -(card.ability.extra.scoring / 2)
+                end
+                if scoreret == 9 or scoreret == 10 then
+                    ret.dollars = card.ability.extra.scoring
+                end
                 return ret
-                else
-            SMODS.scale_card(card, {
-                ref_table = card.ability.extra,
-                ref_value = "scoring",
-                scalar_value = "gain",
-                operation = "X",
-                scaling_message = {
-                message = "+" ..(card.ability.extra.scoring * card.ability.extra.gain).. " Value",
-                colour = G.C.DARK_EDITION
-            }})
+            else
+                SMODS.scale_card(card, {
+                    ref_table = card.ability.extra,
+                    ref_value = "scoring",
+                    scalar_value = "gain",
+                    operation = "X",
+                    scaling_message = {
+                        message = "+" .. (card.ability.extra.scoring * card.ability.extra.gain) .. " Value",
+                        colour = G.C.DARK_EDITION
+                    }
+                })
+            end
         end
-    end
-    if context.after then
-        if card.ability.extra.trigger then
-        card.ability.extra.scoring = card.ability.immutable.revert
-        card.ability.extra.trigger = false
-            return { message = localize("k_reset") }
-    end
-    end
-end,
-    can_use = function(self,card)
+        if context.after then
+            if card.ability.extra.trigger then
+                card.ability.extra.scoring = card.ability.immutable.revert
+                card.ability.extra.trigger = false
+                return { message = localize("k_reset") }
+            end
+        end
+    end,
+    can_use = function(self, card)
         return G.GAME.blind.in_blind
     end,
-    keep_on_use = function(self,card)
+    keep_on_use = function(self, card)
         return true
     end,
     use = function(self, card, area, copier)
         card.ability.extra.trigger = true
-                    G.E_MANAGER:add_event(Event({
-						trigger = 'before',
-						delay = 0.5 + math.random() * 0.4,
-						func = function()
-							play_sound('gong',1, 0.5)
-							card:juice_up(1, 0.2)
-							return true
-                        end
-						}))
-
+        G.E_MANAGER:add_event(Event({
+            trigger = 'before',
+            delay = 0.5 + math.random() * 0.4,
+            func = function()
+                play_sound('gong', 1, 0.5)
+                card:juice_up(1, 0.2)
+                return true
+            end
+        }))
     end
 }
 
 FishAndChips.Fish {
-	key = "dupli",
-	atlas = "pnf_dupli",
-	pos = { x = 0, y = 0 },
-	soul_pos = { x = 1, y = 0 },
+    key = "dupli",
+    atlas = "pnf_dupli",
+    pos = { x = 0, y = 0 },
+    soul_pos = { x = 1, y = 0 },
     weight = 1,
     blueprint_compat = true,
-	ppu_coder = { "FirstTry" },
-	ppu_artist = { "FirstTry" },
-	attributes = { "mult", "chips", "xmult", "economy" },
-	config = {
-		extra = {
-			mult = 0,
+    ppu_coder = { "FirstTry" },
+    ppu_artist = { "FirstTry" },
+    attributes = { "mult" },
+    config = {
+        extra = {
+            mult = 0,
             mult_mod = 2,
-		},
+        },
         immutable = {
             revert = 0
         }
-	},
-	environments = {
+    },
+    environments = {
         wormhole = 1,
         backroom = 1
     },
-	loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.mult, card.ability.extra.mult_mod, colours = { HEX("4db1f6") } } }
-	end,
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.mult, card.ability.extra.mult_mod, colours = { HEX("4db1f6") } } }
+    end,
 
     calculate = function(self, card, context)
-           if context.end_of_round and context.main_eval then
-                card.ability.extra.mult = card.ability.immutable.revert
-                return { message = localize("k_reset") }
-            end
+        if context.end_of_round and context.main_eval then
+            card.ability.extra.mult = card.ability.immutable.revert
+            return { message = localize("k_reset") }
+        end
         if context.individual and context.cardarea == G.play then
             SMODS.scale_card(card, {
                 ref_table = card.ability.extra,
                 ref_value = "mult",
                 scalar_value = "mult_mod",
                 scaling_message = {
-                message = "+" ..(card.ability.extra.mult * card.ability.extra.mult_mod).. " Mult",
-                colour = G.C.MULT
-            }
+                    message = "+" .. (card.ability.extra.mult * card.ability.extra.mult_mod) .. " Mult",
+                    colour = G.C.MULT
+                }
             })
         end
         if (context.joker_main and (to_big(card.ability.extra.mult) > 1)) or context.forcetrigger then
             return {
                 mult = card.ability.extra.mult
             }
+        end
+    end,
+}
+
+FishAndChips.Fish {
+    key = "pixelfish",
+    atlas = "pnf_pixelfish",
+    pos = { x = 0, y = 0 },
+    weight = 5,
+    blueprint_compat = true,
+    ppu_coder = { "Pixel" },
+    ppu_artist = { "FirstTry" },
+    attributes = { "chips", "chipgain", "xchips", "xchipgain", "sellamount", "sellgoal" },
+    config = {
+        extra = {
+            chips = 0,
+            xchips = 1,
+            chipgain = 5,
+            xchipgain = 0.05,
+        },
+        immutable = {
+            sellamount = 0,
+            sellgoal = 3,
+        }
+    },
+    environments = {
+        city_river = 5,
+        wormhole = 1
+    },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.chips, card.ability.extra.xchips, card.ability.extra.chipgain, card.ability.extra.xchipgain, card.ability.immutable.sellamount, card.ability.immutable.sellgoal } }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main then return { chips = card.ability.extra.chips, xchips = card.ability.extra.xchips } end
+        if context.selling_card then
+            SMODS.scale_card(card, {
+                ref_table = card.ability.extra,
+                ref_value = "chips",
+                scalar_value = "chipgain",
+                scaling_message = {
+                    message = "+" .. (card.ability.extra.chips + card.ability.extra.chipgain) .. " Chips",
+                    colour = G.C.CHIPS
+                }
+            })
+            card.ability.immutable.sellamount = card.ability.immutable.sellamount + 1
+            if card.ability.immutable.sellamount > card.ability.immutable.sellgoal - 1 then
+                SMODS.scale_card(card, {
+                    ref_table = card.ability.extra,
+                    ref_value = "xchips",
+                    scalar_value = "xchipgain",
+                    scaling_message = {
+                        message = "+" .. (card.ability.extra.xchips + card.ability.extra.xchipgain) .. " XChips",
+                        colour = G.C.CHIPS
+                    }
+                })
+            card.ability.immutable.sellamount = 0
+            end
         end
     end,
 }
---
+++ localization/pnf/en-us.lua
@@ -0,0 +1,64 @@
+
+return {
+    descriptions = {
+        fac_Fish = {
+            fish_fac_blueax = {
+                name = "Suspicious Blue Axolotl",
+                text = {
+                    {"This {V:1}Fish{} gains",
+                    "{C:white,X:dark_edition}X#2#{} Scoring Value",
+                    "{C:attention}when scoring",},
+                    {"{C:attention}Use{} this {V:1}Fish{} to give a random",
+                    "{C:attention}scoring effect{} equal to {C:dark_edition}#1#",
+            "in the {C:attention}next hand"},
+            {"Resets after scoring"}
+                },
+                flavor = {
+                    [["I'm sorry for existing"]]
+                }
+            },
+            fish_fac_dupli = {
+                name = "Barramun-Duplicare",
+                text = {
+                    "This {V:1}Fish{} gains",
+                    "{C:mult}+#2#{} Mult when",
+                    "a {C:attention}playing card{} is scored",
+                    "and resets at the",
+                    "{C:attention}end of every round",
+                    "{C:inactive}(Currently: {C:mult}+#1#{C:inactive})"
+                },
+                flavor = {
+                    [["I'm totally not who you think i am"]]
+                }
+            },
+            fish_fac_pixelfish = {
+                name = "Pixish",
+                text = {
+                    "This {V:1}Fish{} gains {C:chips}+#3#{} Chips when",
+                    "a {C:attention}card{} is sold",
+                    "Gains {X:chips,C:white}X#4#{} {C:chips}Chips{} for",
+                    "every {C:attention}#6#{} {C:inactive}(#5#){} cards sold",
+                    "{C:inactive}(Currently: {C:chips}+#1#{}, {X:chips,C:white}X#2#{C:inactive})"
+                },
+                flavor = {
+                    [[Active explosion risk.]]
+                }
+            },
+        },
+        PotatoPatch = {
+            PotatoPatchDev_FirstTry = {
+                name = 'FirstTry',
+                text = {
+                    "yo."
+                }
+            },
+            PotatoPatchDev_Pixel = {
+                name = 'Pixel',
+                text = {
+                    "{s:2,C:planet}pi xle LL e",
+                    "I am the one who is computer,.,"
+                },
+            }
+        }
+    }
+}
