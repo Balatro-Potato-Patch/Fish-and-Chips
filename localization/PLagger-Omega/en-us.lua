d25251cf14c4c94202c8bfd53b079c81d0145472
PLagger <plagger1317@gmail.com>
2026-07-31 02:49:19 +0200

    add hawaii fish

---
+++ localization/PLagger-Omega/en-us.lua
@@ -0,0 +1,23 @@
+return {
+    descriptions = {
+        fac_Fish = {
+            fish_fac_plaggeromega_hawaii = {
+                name = 'Humuhumunukunukuāpua\'a Pt II',
+                text = {
+                    '{X:mult,C:white}X#1#{} Mult if a',
+                    '{C:attention}#2#{} was',
+                    '{C:attention}discarded{} this round'
+                },
+                flavor = {
+                    'gay ass clown'
+                }
+            }
+        }
+    },
+    misc = {
+        dictionary = {
+            fac_plaggeromega_active_ex = 'Active!',
+            fac_plaggeromega_inactive = 'Inactive'
+        }
+    }
+}
