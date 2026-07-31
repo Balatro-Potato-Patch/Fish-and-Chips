0b28411e1c8054264eca1153e00242846a2e7b77
Foo54 <157067370+Foo54@users.noreply.github.com>
2026-07-31 10:20:12 -0400

    badge_key

--- src/fish.lua
+++ src/fish.lua
@@ -22,7 +22,7 @@ FishAndChips.Fish = SMODS.Center:extend {
 		G.P_CENTER_POOLS[self.set] = {}
 	end,
 	set_card_type_badge = function(self, card, badges)
-		badges[#badges + 1] = create_badge(localize("k_fac_fish"), FishAndChips.C.FISH, G.C.WHITE, 1.2)
+		badges[#badges + 1] = create_badge(localize(card.config.center.badge_key or "k_fac_fish"), FishAndChips.C.FISH, G.C.WHITE, 1.2)
 	end,
 	post_inject_class = function(self)
 		FishAndChips.verify_submissions()
