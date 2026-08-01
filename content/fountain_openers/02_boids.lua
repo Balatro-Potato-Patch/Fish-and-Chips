05e73755480f30e2593173c04fdb76f47aee9d06
InvalidOS <35383068+InvalidOS@users.noreply.github.com>
2026-08-01 16:38:00 -0400

    pier

--- content/fountain_openers/02_boids.lua
+++ content/fountain_openers/02_boids.lua
@@ -286,11 +286,12 @@ FishAndChips.Fish {
 	perishable_compat = false,
 	ppu_coder = { "Alexi" },
 	ppu_artist = { "Grahkon" },
-	attributes = { "chips" }, -- placeholder
+	attributes = { "chips" },
 	config = {
 	},
 	environments = {
 		wormhole = 1,
+		pier = 1,
 	},
 	loc_vars = function(self, info_queue, card)
 		-- return { vars = { card.ability.extra.chips } }
