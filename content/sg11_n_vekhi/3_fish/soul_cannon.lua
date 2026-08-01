56ae511de49b38478b284729426c2b5305cb4b5d
SleepyG11 <makatymba2001@yandex.ru>
2026-08-01 04:51:50 +0300

    Soul Cannonfish sprite (need cook more on it)

--- content/sg11_n_vekhi/3_fish/soul_cannon.lua
+++ content/sg11_n_vekhi/3_fish/soul_cannon.lua
@@ -1,7 +1,14 @@
+SMODS.Atlas({
+	key = "sg11_n_vekhi_soul_cannon",
+	path = "sg11_n_vekhi/soul_cannon.png",
+	px = 71,
+	py = 95,
+})
+
 FishAndChips.Fish({
 	key = "sg11_n_vekhi_soul_cannon",
-	-- atlas = "fish",
-	-- pos = { x = 0, y = 0 },
+	atlas = "fac_sg11_n_vekhi_soul_cannon",
+	pos = { x = 0, y = 0 },
 	ppu_coder = { "sleepyg11" },
 	ppu_artist = { "vevekhi" },
 	attributes = {},
@@ -53,6 +60,20 @@ FishAndChips.Fish({
 								- target_amount
 						end,
 					}
+				end
+			end
+		end
+	end,
+	update = function(self, card, dt)
+		if G.GAME then
+			local atlas_x = card.children.center.sprite_pos.x
+			if G.GAME.current_round.hands_left <= 1 then
+				if atlas_x ~= 1 then
+					card.children.center:set_sprite_pos({ x = 1, y = 0 })
+				end
+			else
+				if atlas_x ~= 0 then
+					card.children.center:set_sprite_pos({ x = 0, y = 0 })
 				end
 			end
 		end
