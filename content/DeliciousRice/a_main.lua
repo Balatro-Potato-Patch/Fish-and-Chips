fb039809a7de4887ad44eb6b671bcadfea88100e
cheekyrotter <george0406@outlook.com>
2026-08-02 11:32:18 +0100

    Basic setup (unfinished but I need to access this from my laptop during a trip away)

---
+++ content/DeliciousRice/a_main.lua
@@ -0,0 +1,21 @@
+PotatoPatchUtils.Developer({
+	name = 'cheekyrotter',
+	atlas = 'fac_cards',
+	colour = G.C.RED,
+	fac_partner = 'EDriGO'
+})
+
+PotatoPatchUtils.Developer({
+	name = 'EDriGO',
+	atlas = 'fac_cards',
+	pos = {x = 1, y = 0},
+	colour = G.C.GREEN,
+	fac_partner = 'cheekyrotter'
+})
+
+SMODS.Atlas({
+	key = "delrice_fish", -- Please include your name/team name in your atlas keys
+	path = "DeliciousRice/fish.png",
+	px = 71,
+	py = 95,
+})
