b15b52c49d7ec73f2e311d948461da1ecee7e919
Breuhh <breuhh69@gmail.com>
2026-08-02 09:03:02 +0200

    Add files via upload

---
+++ breukel/en-us.lua
@@ -0,0 +1,88 @@
+return {
+    descriptions = {
+        fac_Fish = {
+            fish_fac_markerel = {
+                name = "Markerel",
+                text = {
+                    {"Upon use:",
+                    "{C:attention}Creates{} a random {C:attention}Tag",
+                    "Applies Eternal to",
+                    "a random {C:attention}Joker{} in hand",
+                    "{C:attention}Creates{} a second tag",
+                    "if {C:fac_breukel_overtime,E:1}Overtime{} is more",
+                    "than or equal to {C:attention}8"},
+                    {"{C:attention}+#1#{} to {C:fac_breukel_overtime,E:1}Overtime{}",
+                    "{C:inactive}(Overtime: #2#/#3#){}"}
+                },
+                flavor = {
+                    "Its like a marker",
+                    "from the office"
+                }
+            },
+
+            fish_fac_ceo = {
+                name = {"Chief Executive","Oarfisher"},
+                text = {
+                    {"{C:fac_breukel_overtime,E:1}Overtime{} can go up to {C:attention}20",
+                    "Every {C:attention}Fish{} gives an extra",
+                    "{C:attention}+#1#{} to {C:fac_breukel_overtime,E:1}Overtime{} when triggered"},
+                    {"{C:attention}+#2#{} to {C:fac_breukel_overtime,E:1}Overtime{}",
+                    "{C:inactive}(Overtime: #3#/#4#){}"}
+                },
+                flavor = {
+                    "Money money money",
+                }
+            },
+
+            fish_fac_stockfish = {
+                name = "Stockfish",
+                text = {
+                    {"Gives between {C:fac_sand_dollars}-#1#{} and {C:fac_sand_dollars}+#1#",
+                    "Sand dollars at the {C:attention}end",
+                    "{C:attention}of the round{}, higher chance",
+                    "for {C:fac_sand_dollars}+Sand dollars{} the higher",
+                    "{C:fac_breukel_overtime,E:1}Overtime{} is"
+                    },
+                    {"{C:attention}+#2#{} to {C:fac_breukel_overtime,E:1}Overtime{}",
+                    "{C:inactive}(Overtime: #3#/#4#){}"}
+                },
+                flavor = {
+                    "Not associated",
+                    "with chess!"
+                }
+            },
+
+            fish_fac_employeel = {
+                name = "Employeel",
+                text = {
+                    {"Gives {C:mult}Mult{} equal",
+                    "to {C:fac_breukel_overtime,E:1}Overtime"},
+                    {"{C:attention}+#1#{} to {C:fac_breukel_overtime,E:1}Overtime{}",
+                    "{C:inactive}(Overtime: #2#/#3#){}"}
+                },
+                flavor = {
+                    "9 to 5 sure is",
+                    "boring, ey?"
+                }
+            },
+
+            fish_fac_codcument = {
+                name = "Codcument",
+                text = {
+                    {"{C:attention}Transforms{} into the",
+                    "next obtained {C:attention}Fish",
+                    "{C:attention}Create{} a copy if",
+                    "{C:fac_breukel_overtime,E:1}Overtime{} is exactly",
+                    "{C:attention}10"},
+                    {"{C:attention}+#1#{} to {C:fac_breukel_overtime,E:1}Overtime{}",
+                    "{C:inactive}(Overtime: #2#/#3#){}"}
+                },
+                flavor = {
+                    "I got no idea",
+                    "for this text hi :)"
+                }
+            },
+        },
+
+    }
+}
---
+++ breukel/fish.lua
@@ -0,0 +1,205 @@
+FishAndChips.Fish {
+	key = "employeel",
+	atlas = "fac_breukel_fish",
+	pos = {x = 1,y = 0},
+	weight = 8,
+	stats = {weight = {min = 0.05, max = 0.086}, length = {min = 1.4, max = 1.7}},
+	ppu_coder = {"Breuhh"},
+	ppu_artist = {"Comykel"},
+	attributes = {"mult"},
+
+	environments = {
+		city_river = 10,
+		styx = 5
+	},
+	config = {
+		extra = {
+			Overtime = 0.5,
+		}
+	},
+
+	loc_vars = function(self, info_queue, card)
+		return {vars = {
+			card.ability.extra.Overtime,
+			G.fac_Breukel.GetOverTime()
+		}}
+	end,
+
+	calculate = function(self, card, context)
+		if context.joker_main then
+			G.fac_Breukel.AddOverTime(card, card.ability.extra.Overtime)
+			return {mult = G.fac_Breukel.GetOverTime()}
+		end
+	end,
+}
+
+FishAndChips.Fish {
+	key = "ceo",
+	atlas = "fac_breukel_fish",
+	pos = {x = 2,y = 0},
+	weight = 4,
+	ppu_coder = {"Breuhh"},
+	ppu_artist = {"Comykel"},
+	attributes = {"passive"},
+
+	environments = {
+		city_river = 10,
+	},
+	config = {
+		extra = {
+			Overtime = 1,
+			extraOvertime = 0.5
+		}
+	},
+
+	loc_vars = function(self, info_queue, card)
+		return {vars = {
+			card.ability.extra.extraOvertime,
+			card.ability.extra.Overtime,
+			G.fac_Breukel.GetOverTime()
+		}}
+	end,
+
+	calculate = function(self, card, context)
+		if context.other_unknown and context.other_unknown.ability.set == "fac_Fish" then
+			G.fac_Breukel.AddOverTime(context.other_unknown, card.ability.extra.extraOvertime)
+		end
+	end,
+
+    remove_from_deck = function(self, card, from_debuff)
+        G.GAME.fac_Breukel.MaxOverTime = 10
+    end,
+
+	on_catch = function(self, card)
+		G.GAME.fac_Breukel.MaxOverTime = 20
+	end,
+    add_from_deck = function(self, card, from_debuff)
+        G.GAME.fac_Breukel.MaxOverTime = 20
+    end,
+}
+
+FishAndChips.Fish {
+	key = "markerel",
+	atlas = "fac_breukel_markerel",
+	pos = {x = 0,y = 0},
+	weight = 6,
+	ppu_coder = {"Breuhh"},
+	ppu_artist = {"Comykel"},
+	attributes = {"generation","usable"},
+
+	environments = {
+		city_river = 10,
+		backroom = 2.5,
+		pier = 1
+	},
+	config = {
+		extra = {
+			Overtime = 1
+		}
+	},
+
+	loc_vars = function(self, info_queue, card)
+		return {vars = {
+			card.ability.extra.Overtime,
+			G.fac_Breukel.GetOverTime()
+		}}
+	end,
+
+	use = function(self, card)
+		add_tag({key = pseudorandom_element(G.P_TAGS, 'Markerel').key})
+		if G.GAME.fac_Breukel.OverTime >= 8 then 
+			add_tag({key = pseudorandom_element(G.P_TAGS, 'Markerel').key}) 
+		end
+
+		local Joker = pseudorandom_element(G.jokers.cards, 'Markerel Eternal')
+		Joker:add_sticker('eternal', true)
+		Joker:juice_up()
+
+		G.fac_Breukel.AddOverTime(card, card.ability.extra.Overtime)
+	end,
+	can_use = function(self, card)
+		if G.jokers and #G.jokers.cards > 0 then return true end
+	end,
+
+    on_catch = function(self, card)
+        card.ability.skin = pseudorandom('Marker',0,7)
+        card.children.center:set_sprite_pos{x = card.ability.skin, y = 0}
+    end,
+}
+
+FishAndChips.Fish {
+	key = "stockfish",
+	atlas = "fac_breukel_fish",
+	pos = {x = 0,y = 0},
+	weight = 6,
+	ppu_coder = {"Breuhh"},
+	ppu_artist = {"Comykel"},
+	attributes = {"economy"},
+
+	environments = {
+		city_river = 10,
+		backroom = 4,
+		wormhole = 8
+	},
+	config = {
+		extra = {
+			Overtime = 1.5,
+			Sand_dollars = 2
+		}
+	},
+
+	loc_vars = function(self, info_queue, card)
+		return {vars = {
+			card.ability.extra.Sand_dollars/2,
+			card.ability.extra.Overtime,
+			G.fac_Breukel.GetOverTime()
+		}}
+	end,
+
+	calculate = function(self, card, context)
+		if context.end_of_round and context.main_eval then
+			G.fac_Breukel.AddOverTime(card, card.ability.extra.Overtime)
+
+			local amount = pseudorandom('stockfish', 0, card.ability.extra.Sand_dollars)/2
+			local Overtime, Max = G.fac_Breukel.GetOverTime()
+			if pseudorandom('chessfish') < 0.25 + (Overtime/Max)/(4/3) then
+				return {sand_dollars = amount}
+			else
+				return {sand_dollars = -amount}
+			end
+		end
+	end,
+}
+
+FishAndChips.Fish {
+	key = "codcument",
+	atlas = "fac_breukel_fish",
+	pos = {x = 0,y = 1},
+	weight = 8,
+	ppu_coder = {"Breuhh"},
+	ppu_artist = {"Comykel"},
+	attributes = {"mult"},
+
+	environments = {
+		city_river = 10,
+		backroom = 6
+	},
+	config = {
+		extra = {
+			Overtime = 1,
+		}
+	},
+
+	loc_vars = function(self, info_queue, card)
+		return {vars = {
+			card.ability.extra.Overtime,
+			G.fac_Breukel.GetOverTime()
+		}}
+	end,
+
+	calculate = function(self, card, context)
+		if context.fac_fish_hooked then
+			print("heyy")
+		end
+	end,
+}
