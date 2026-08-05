d7c26122395f547445dad72eb96c5b463a0ef18c
CampfireCollective <122759859+GuilloryCraft@users.noreply.github.com>
2026-08-05 15:57:55 -0700

    Add files via upload

---
+++ localization/CCitty/en-us.lua
@@ -0,0 +1,181 @@
+return {
+    descriptions = {
+        fac_Fish = {
+            fish_fac_fihs_CCitty = {
+                name = "fihs.....",
+                text = {
+                    "{element:1}"
+                },
+                flavor = {
+                    "#1#"
+                }
+            },
+            fish_fac_perkoio = {
+                name = "Perkoio",
+                text = {
+                    "Create a Negative copy",
+                    "of a random Joker every",
+                    "4 rounds"
+                },
+                flavor = {
+                    "A legendary fish. Its swimming patterns are",
+                    "unpredictable, likely due to centuries of",
+                    "alcohol being poured into its home waters."
+                }
+            },
+             fish_fac_yoray = {
+                name = "Yoray Eel",
+                text = {
+                    "each discarded card",
+                    "gains {C:white,X:red}X#1#{} Mult"
+                },
+                flavor = {
+                    "A legendary fish. Despite their infinite jest",
+                    "and flashes of merriment, these creatures often",
+                    "fail to live for more than 23 years."
+                }
+            },
+             fish_fac_canioctopus = {
+                name = "Canioctopus",
+                text = {
+                    "Create a random Bait when",
+                    "any card is destroyed"
+                },
+                flavor = {
+                    "A legendary fish. It struggles to look at other fish,",
+                    "perhaps in jealousy or in shame. It would explain it's pale appearance..."
+                }
+            },
+             fish_fac_troutulet = {
+                name = "Troutulet",
+                text = {
+                    "Played Kings and Queens",
+                    "{C:fac_sand_dollars,f:fac_sand_dollars}${C:fac_sand_dollars}#1#{} when scored",
+                    "and are retriggered"
+                },
+                flavor = {
+                    "A legendary fish. Not much is known about this species,",
+                    "though it's often associated with stories of mischief and mayhem."
+                }
+            },
+             fish_fac_chicod = {
+                name = "Chicod",
+                text = {
+                    "When any card is {C:attention}sold",
+                    "during a {C:attention}blind{}, reduce",
+                    "blind requirement by {C:red}X#1#"
+                },
+                flavor = {
+                    "A legendary fish. Fishermen often struggle to catch them",
+                    "due to their sharp wit, cooperative hunting and rapier-like rostrum."
+                }
+            },
+             fish_fac_drspectred = {
+                name = "Doctor Sharktred",
+                text = {
+                    "temp"
+                },
+                flavor = {
+                    "This one lost it's tail during a fight in a Cracker Barrel",
+                    "parking lot. A group of these is called a University."
+                }
+            },
+             fish_fac_seiunsky = {
+                name = "Seiun Skyhorse",
+                text = {
+                    "temp"
+                },
+                flavor = {
+                    "Free as a drifting cloud... don't be fooled by it's laziness,",
+                    "it is capable of winning through tricky strategies."
+                }
+            },
+             fish_fac_garfieldphone = {
+                name = "Garfield Phone",
+                text = {
+                    "temp"
+                },
+                flavor = {
+                    "Rumors are that a lost shipping container is spewing out these catty",
+                    "communication devices. They're hard to find on Mondays, for some strange reason."
+                }
+            },
+            fish_fac_bluebell_angler = {
+                name = "Bluebell Angler",
+                text = {
+                    "Forces 1 card to always be selected",
+                    "until first hand played each round"
+                },
+                {
+                    "All scoring cards in first hand of",
+                    "round gain random perma-bonuses if",
+                    "the forced card is in scoring hand"
+                },
+                flavor = {
+                    "Whilst the seven frolick in the sunset, this one lurks in the depths",
+                    "of the ocean, hoping to someday witness the golden one."
+                }
+            },
+            fish_fac_solinseaslug = {
+                name = "#3# the Sea Slug",
+                text = {
+                    "Earn $#1# when any Fish is sold,",
+                    "destroyed after #2# Fish sold"
+                },
+                flavor = {
+                    "#4#",
+                    "#5#"
+                }
+            },
+        },
+    },
+    misc = {
+        quips = {
+            CCitty_test = {
+                "Hello. I am a flaming homosexual,",
+                "and I've played 5000 hours of Balatro"
+            },
+            CCitty_tip1 = {
+                "Did you know? The legendary",
+                "Troutulet can be found in the City!"
+            },
+            CCitty_tip2 = {
+                "Did you know? The legendary",
+                "Perkoio can be found in the Soup!",
+            },
+            CCitty_tip3 = {
+                "Did you know? The legendary",
+                "Canioctopus can be found in the volcano!",
+            },
+            CCitty_tip4 = {
+                "Did you know? The legendary",
+                "Yoray Eel can be found in the Styx!",
+            },
+            CCitty_tip5 = {
+                "Did you know? The legendary",
+                "Chicod can be found in the city!",
+            },
+            CCitty_welcomeback = {
+                "Alright folks, welcome back",
+            },
+            CCitty_nooo = {
+                "Noooo....",
+            },
+            CCitty_loss = {
+                "The muhnee....."
+            },
+            CCitty_gain = {
+                "Yaaasss~"
+            },
+            CCitty_gameover = {
+                "That's pretty stinky."
+            },
+            CCitty_calc = {
+                "Calculated!"
+            },
+            CCitty_neverpunished = {
+                "Never punished"
+            },
+        }
+    }
+}
