4b371b93d4b237bce4d0eaa2ca46735ad957f83f
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2026-08-05 16:10:51 -0400

    fission chip

---
+++ content/minty/fissionchip.lua
@@ -0,0 +1,53 @@
+FishAndChips.Fish{
+    key = "minty_fission",
+    atlas = "minty_fish",
+    pos = {x=20, y=0},
+    weight = 1,
+    ppu_coder = {"minty"},
+    ppu_artist = {"?"},
+    environments = { --Maximum 6
+        aquifer = 5,
+        backroom = 10,
+        wormhole = 10,
+        city_river = 5,
+        --[[
+        calm_pond = 10,
+        chocolate_river = 10,
+        styx = 10,
+        pier = 10,
+        swamp = 10,
+        volcano = 10,
+        soup = 10,
+        garden = 10,
+        --]]
+    },
+    attributes = {
+        "retrigger", "chance"
+    },
+    stats = {
+        weight = { min = 0.005, max = 0.005}, --In kilograms
+        length = { min = 0.02, max = 0.02}, --In meters
+    },
+    config = {
+        extra = {
+            luck = 1,
+            odds = 2,
+            retriggers = 1
+        }
+    },
+    loc_vars = function (self, info_queue, card)
+        local luck, odds = SMODS.get_probability_vars(card, card.ability.extra.luck, card.ability.extra.odds, "minty_fac_fission_retrigger", false)
+        
+        return {
+            luck, odds,
+            card.ability.extra.retriggers, card.ability.extra.retriggers ~= 1 and "s" or ""
+        }
+    end,
+    calculate = function (self, card, context)
+        if context.retrigger_joker and context.other_card ~= card and context.other_card.config.center.set == "fac_Fish" and SMODS.pseudorandom_probability(card, "minty_fac_fission_retrigger", card.ability.extra.luck, card.ability.extra.odds) then
+            return {
+                repetitions = card.ability.extra.retriggers
+            }
+        end
+    end
+}
