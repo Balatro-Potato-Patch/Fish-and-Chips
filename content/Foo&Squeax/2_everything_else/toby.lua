363bec00bf1fa057a5f0315118c801c7a872b2e5
Foo45 <rowan.stjames.cooper@gmail.com>
2026-08-05 22:38:46 -0400

    doggo

--- content/Foo&Squeax/2_everything_else/toby.lua
+++ content/Foo&Squeax/2_everything_else/toby.lua
@@ -145,8 +145,8 @@ function G.UIDEF.card_h_popup(card)
 							r_bond = "Weak",
 							instance_type = "POPUP",
 							offset = {
-								x = math.random() * card.children.h_popup.VT.w - 1,
-								y = math.random() * card.children.h_popup.VT.h - 1
+								x = math.random() * (card.children.h_popup.VT.w - 1),
+								y = math.random() * (card.children.h_popup.VT.h - 1)
 							}
 						}
 					}
@@ -228,3 +228,56 @@ function G.FUNCS.fac_fas_toby_update (e)
 		end
 	end
 end
+
+
+function G.FUNCS.fac_fas_toby_nuhuh (e)
+	G.FUNCS.can_sell_card(e)
+	if e.states.hover.is then
+		if not e.config.fac_fas_hovered then
+			e.config.ref_table.children.use_button:set_role{
+				
+				r_bond = "Weak"
+			}
+			local target = {
+				x = 10 * (math.random() - 0.5),
+				y = 10 * (math.random() - 0.5)
+			}
+			ease_value(e.config.ref_table.children.use_button.alignment.offset, "x", target.x - e.config.ref_table.children.use_button.alignment.offset.x, nil, nil, true)
+			ease_value(e.config.ref_table.children.use_button.alignment.offset, "y", target.y - e.config.ref_table.children.use_button.alignment.offset.y, nil, nil, true)
+			e.config.ref_table.children.use_button.T.r = 2 * math.pi * math.random()
+		end
+	else
+		e.config.fac_fas_hovered = nil
+	end
+end
+
+local use_and_sell = G.UIDEF.use_and_sell_buttons
+---@diagnostic disable-next-line: duplicate-set-field
+function G.UIDEF.use_and_sell_buttons(card)
+	local ret = use_and_sell(card)
+	if card.config.center.key == "fish_fac_fas_toby_fish" then
+		local sell = {n=G.UIT.C, config={align = "cr"}, nodes={
+			{n=G.UIT.C, config={ref_table = card, align = "cr",padding = 0.1, r=0.08, minw = 1.25, hover = true, shadow = true, colour = G.C.UI.BACKGROUND_INACTIVE, one_press = true, button = 'sell_card', func = 'fac_fas_toby_nuhuh', handy_insta_action = 'sell'}, nodes={
+				{n=G.UIT.B, config = {w=0.1,h=0.6}},
+				{n=G.UIT.C, config={align = "tm"}, nodes={
+					{n=G.UIT.R, config={align = "cm", maxw = 1.25}, nodes={
+						{n=G.UIT.T, config={text = localize('b_sell'),colour = G.C.UI.TEXT_LIGHT, scale = 0.4, shadow = true}}
+					}},
+					{n=G.UIT.R, config={align = "cm"}, nodes={
+						{n=G.UIT.T, config={text = localize('$'),colour = G.C.WHITE, scale = 0.55, shadow = true, font = SMODS.Fonts["fac_sand_dollars"]}},
+						{n=G.UIT.T, config={ref_table = card, ref_value = 'sell_cost_label',colour = G.C.WHITE, scale = 0.55, shadow = true}}
+					}}
+				}}
+			}},
+		}}
+		ret = {
+			n=G.UIT.ROOT, config = {padding = 0, colour = G.C.CLEAR}, nodes={
+				{n=G.UIT.C, config={padding = 0.15, align = 'cl'}, nodes={
+					{n=G.UIT.R, config={align = 'cl'}, nodes={
+							sell
+					}},
+				}},
+			}}
+		end
+		return ret
+	end
