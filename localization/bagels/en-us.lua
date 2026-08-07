8a345541f1f9ddb039555fca7df186d995509ad3
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2026-08-07 18:25:46 -0400

    Add A for Effish

--- localization/bagels/en-us.lua
+++ localization/bagels/en-us.lua
@@ -37,11 +37,20 @@ return {
 				text = { '{C:attention}+#1#{} Bait when any', 'Booster Pack is opened' },
 				flavor = { 'I think this is the', 'same worm you used', 'as bait. It must really', 'like your hook.' },
 			},
+			fish_fac_bagels_a_for_effish = {
+				name = '{element:6} for Effish',
+				text = {
+					'Held {C:attention}{element:1}ces{} {element:2}re',
+					'{element:3}{C:attention}dded{} to pl{element:4}yed h{element:5}nd',
+				},
+				flavor = { 'This fish seems to', 'like drinking coco{element:1}.' },
+			},
 		},
 	},
 	misc = {
 		dictionary = {
 			k_fac_bagels_plus_bait = '+1 Bait',
+			k_fac_bagels_a = 'A',
 		},
 	},
 }
