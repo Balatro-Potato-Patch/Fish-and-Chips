28591b9bd854017b12c97fc25b8dcd3d5d1c43a0
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2026-08-07 14:38:28 -0400

    goldfish art

--- content/minty/000lib.lua
+++ content/minty/000lib.lua
@@ -10,6 +10,23 @@ SMODS.Atlas{
     py = 95
 }
 
+SMODS.Atlas{
+    key = "minty_lineboilfish",
+    path = "minty/animated fish.png",
+    px = 71,
+    py = 95,
+    atlas_table = "ANIMATION_ATLAS",
+    frames = 2,
+    fps = 4
+}
+
+SMODS.Atlas{
+    key = "minty_nolineboilfish",
+    path = "minty/animated fish.png",
+    px = 71,
+    py = 95,
+}
+
 SMODS.Attribute{ --Cards which do nothing
     key = "nothing",
     alias = {
