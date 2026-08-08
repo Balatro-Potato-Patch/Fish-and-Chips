c1c705a7c11f1e52865dc5546cc55b126dc9c4b0
Breuhh <breuhh69@gmail.com>
2026-08-08 16:58:51 +0200

    Add files via upload

--- localization/breukel/en-us.lua
+++ localization/breukel/en-us.lua
@@ -55,7 +55,7 @@ return {
             fish_fac_employeel = {
                 name = "Employeel",
                 text = {
-                    {"Gives {C:mult}Mult{} equal",
+                    {"Gives {C:mult}+Mult{} equal",
                     "to {C:fac_breukel_overtime,E:1}Overtime"},
                     {"{C:attention}+#1#{} to {C:fac_breukel_overtime,E:1}Overtime{}",
                     "{C:inactive}(Overtime: #2#/#3#){}"}
@@ -63,6 +63,20 @@ return {
                 flavor = {
                     "9 to 5 sure is",
                     "boring, ey?"
+                }
+            },
+
+            fish_fac_plecoworker = {
+                name = "Plecoworker",
+                text = {
+                    {"Gives {C:chips}+Chips{} equal",
+                    "to {C:attention}5*{C:fac_breukel_overtime,E:1}Overtime"},
+                    {"{C:attention}+#1#{} to {C:fac_breukel_overtime,E:1}Overtime{}",
+                    "{C:inactive}(Overtime: #2#/#3#){}"}
+                },
+                flavor = {
+                    "Listening to coworker",
+                    "music RIGHT NOW!"
                 }
             },
 
@@ -81,6 +95,37 @@ return {
                     "Containing information about",
                     "the next Potato Patch event!",
                     "{C:red,s:0.8,E:1}DO NOT TOUCH"
+                }
+            },
+
+            fish_fac_pirinter = {
+                name = "Pirinter",
+                text = {
+                    {"If {C:attention}first {C:red}discard{} of round has {C:attention}1{} card:",
+                    "{C:red}Destroy{} the card and {C:attention}create a copy.",
+                    "The copy has a {E:1,C:green}chance{} to gain a random",
+                    "{C:attention}Enhancement{}, {C:attention}Edition{} or {C:attention}Seal.",
+                    "{E:1,C:green}Chances{} increases with {C:fac_breukel_overtime,E:1}Overtime{}"},
+                    {"{C:attention}+#1#{} to {C:fac_breukel_overtime,E:1}Overtime{}",
+                    "{C:inactive}(Overtime: #2#/#3#){}"}
+                },
+                flavor = {
+                    "Comykel is calling",
+                }
+            },
+
+            fish_fac_produck = {
+                name = "Produck",
+                text = {
+                    {"Played and scored cards gives",
+                    "{C:attention}+#1#{} to {C:fac_breukel_overtime,E:1}Overtime{}.",
+                    "Apply Gold to all cards in hand",
+                    "if {C:fac_breukel_overtime,E:1}Overtime{} is more than {C:attention}9"},
+                    {"{C:attention}+#2#{} to {C:fac_breukel_overtime,E:1}Overtime{}",
+                    "{C:inactive}(Overtime: #3#/#4#){}"}
+                },
+                flavor = {
+                    "Breuhh is calling",
                 }
             },
         },
