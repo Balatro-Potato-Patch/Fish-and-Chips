1bec02488d0703d0d38ee9a815bc00ec6bd95525
Eremel <richardbailey@outlook.com>
2026-08-10 22:30:21 +0100

    Dev objects

---
+++ content/radiation_eremel/submission.lua
@@ -0,0 +1,16 @@
+PotatoPatchUtils.Developer({
+	name = 'eremel',
+    loc = true,
+	-- atlas = 'radiation_eremel_credits',
+	colour = HEX('3FC7EB'),
+	fac_partner = 'fac_radiation'
+})
+
+PotatoPatchUtils.Developer({
+	name = 'radiation',
+    loc = true,
+	-- atlas = 'radiation_eremel_credits',
+	pos = {x = 1, y = 0},
+	colour = HEX('FF7C0A'),
+	fac_partner = 'fac_eremel'
+})
---
+++ localization/radiation_eremel/en-us.lua
@@ -0,0 +1,24 @@
+return {
+    descriptions = {
+		PotatoPatch = {
+			PotatoPatchDev_eremel = {
+				name = "Eremel",
+                text = {
+                    {
+                        'Hey! I\'m a part of the organiser team here at the Potato Patch,',
+                        'and I\'m really proud of the work everyone has done on this mod!',
+                        'It\'s been a blast hosting this event and I hope you all enjoy it!'
+                    },
+                    {
+                        'Some of the other mods I\'ve worked on are',
+                        'SMODS, Galdur #1#, Malverk, Ortalab and Monarchy'
+                    }
+                }
+			},
+			PotatoPatchDev_radiation = {
+				name = "RadiationV2",
+			},
+        },
+        
+    }
+}
