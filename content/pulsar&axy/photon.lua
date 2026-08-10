866c6189d96c647d5985fcfb0bafd4d6905e0d46
Axyraandas <62352786+Axyraandas@users.noreply.github.com>
2026-08-10 18:44:37 -0400

    Photon shader given up, set_sprites instead

--- content/pulsar&axy/photon.lua
+++ content/pulsar&axy/photon.lua
@@ -44,10 +44,34 @@ FishAndChips.Fish {
             end
         end
 	end,
-	-- draw = function(self, card, layer)
-	-- 	if self.discovered or card.params.bypass_discovery_center then
-	-- 		card.children.center:draw_shader('fac_pa_photon', nil, card.ARGS.send_to_shader)
-	-- 	end
-	-- end,
+	set_sprites = function(self, card, front)
+		local position = 1
+		local fish_length = card and card.ability and card.ability.stats and card.ability.stats.length / card.ability.stats.units.length.scale or 500
+		fish_length = tonumber(string.format('%.0f', fish_length))
+		if fish_length > 625 then
+			position = 0
+		elseif fish_length > 590 then
+			position = 1
+		elseif fish_length > 565 then
+			position = 2
+		elseif fish_length > 520 then
+			position = 3
+		elseif fish_length > 500 then
+			position = 4
+		elseif fish_length > 435 then
+			position = 5
+		elseif fish_length >= 380 then
+			position = 6
+		end
+
+		if position then
+			card.children.center:set_sprite_pos({x = position, y = 4})
+		end
+	end,
+	draw = function(self, card, layer)
+		if self.discovered or card.params.bypass_discovery_center then
+			card.children.center:draw_shader('booster', nil, card.ARGS.send_to_shader)
+		end
+	end,
 	-- shader = 'pa_photon',
 }
