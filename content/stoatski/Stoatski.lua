799bd35a644ee1a3cb0096e283a64f38358b6b32
Keaton Blomquist <37050207+stoat-ski@users.noreply.github.com>
2026-08-10 09:33:55 -0600

    blarg

--- content/stoatski/Stoatski.lua
+++ content/stoatski/Stoatski.lua
@@ -1,29 +1,30 @@
 PotatoPatchUtils.Developer({
 	name = 'stoatski',
-	--atlas = 'fac_cards',
+	atlas = 'fac_cards',
 	colour = G.C.YELLOW,
 	ignore_limits = false -- USING THIS VALUE WILL RESULT IN YOUR SUBMISSION BEING REJECTED
 })
 
 SMODS.Atlas({
-	key = "stoatfish", -- Please include your name/team name in your atlas keys
-	path = "stoatski/fish.png",
+	key = "stoatskifish", -- Please include your name/team name in your atlas keys
+	path = "stoatski/stoatskifish.png",
 	px = 71,
 	py = 95,
 })
 
 
 FishAndChips.Fish {
-	key = "stoatfish",
+	key = "otter",
 	weight = 10,
-	atlas = "otter",
+	atlas = "stoatskifish",
 	pos = { x = 0, y = 0 },
 	ppu_coder = { "stoatski" },
 	in_pool = function() return false end,
-	no_collection = true,
 	discovered = true,
     environments = {
-		pier = 10,
-		city_river = 2.5
-	},
+		pier = 1000,
+		city_river = 1000,
+		wormhole = 100,
+		calm_pond = 1000
+	}
 }
