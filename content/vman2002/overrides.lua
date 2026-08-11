c24f71f7679297064aa0e770bbbbb37e774e3a30
VMan-2002 <vman.2002.au@gmail.com>
2026-08-11 14:46:16 +1000

    Update overrides.lua

--- content/vman2002/overrides.lua
+++ content/vman2002/overrides.lua
@@ -3,7 +3,7 @@ local play_ref = evaluate_play_main
 function evaluate_play_main(...)
 	G.GAME.fac_trust_active = true
 	local ret = {play_ref(...)}
-	G.GAME.fac_trust_active = false
+	G.GAME.fac_trust_active = nil
 	return unpack(ret)
 end
 
