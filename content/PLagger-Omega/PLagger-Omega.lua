17a2a4d6a293697c846b1d593d5cb7bdeac2051e
PLagger <plagger1317@gmail.com>
2026-08-11 21:06:14 +0200

    B.A.A. shouts out Giada

--- content/PLagger-Omega/PLagger-Omega.lua
+++ content/PLagger-Omega/PLagger-Omega.lua
@@ -367,7 +367,22 @@ FishAndChips.Fish{ --Biblically Accurate Angelfish
   end,
 
   use = function (self, card, area)
-    print'shout out giada'
+    G.E_MANAGER:add_event(Event({
+      func = function ()
+        attention_text({
+          text = localize('fac_plaggeromega_giada'),
+          scale = 1.3,
+          hold = 1.4,
+          major = card,
+          backdrop_colour = G.C.YELLOW,
+          align = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK or G.STATE == G.STATES.SMODS_BOOSTER_OPENED)
+                  and 'tm' or 'cm',
+          offset = { x = 0, y = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK or G.STATE == G.STATES.SMODS_BOOSTER_OPENED) and -0.2 or 0 },
+                        silent = true
+        })
+      return true
+      end
+    }))
     SMODS.add_card({set = 'Joker', rarity = 'Rare', force_stickers = {'rental'}, key_append = 'fac_plaggeromega_baa'})
   end,
 
