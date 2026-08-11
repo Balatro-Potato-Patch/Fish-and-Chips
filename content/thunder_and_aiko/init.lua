5a74b9b1d5324bb00466b7b1993a6135d58d18a6
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2026-08-12 01:25:08 +0700

    chore: finish credit sprite & finalises the fishes

--- content/thunder_and_aiko/init.lua
+++ content/thunder_and_aiko/init.lua
@@ -7,6 +7,15 @@ local thunderedge_gradient = SMODS.Gradient({
 	cycle = 1.5,
 })
 
+local aikoyori_gradient = SMODS.Gradient({
+	key = "aikoyori_gradient",
+	colours = {
+		HEX("600060"),
+		HEX("000000"),
+	},
+	cycle = 3,
+})
+
 SMODS.Sound({
 	key = "moai_catch",
 	path = "thunder_and_aiko/moai.ogg",
@@ -36,10 +45,19 @@ SMODS.Font({
 	TEXT_HEIGHT_SCALE = 0.7
 })
 
+
+SMODS.Atlas({
+	key = "thunder_and_aiko_credits",
+	path = "thunder_and_aiko/thunderedgeaikoyori.png",
+	px = 142,
+	py = 95,
+})
+
+
 PotatoPatchUtils.Developer({
 	name = "thunderedge",
 	loc = true,
-	atlas = "fac_cards",
+	atlas = "fac_thunder_and_aiko_credits",
 	colour = thunderedge_gradient,
 	fac_partner = "fac_aikoyori",
 	joint_credits = 2,
@@ -48,9 +66,9 @@ PotatoPatchUtils.Developer({
 PotatoPatchUtils.Developer({
 	name = "aikoyori",
 	loc = true,
-	atlas = "fac_cards",
+	atlas = "fac_thunder_and_aiko_credits",
 	pos = { x = 1, y = 0 },
-	colour = G.C.YELLOW,
+	colour = aikoyori_gradient,
 	fac_partner = "fac_thunderedge",
 	joint_credits = 2,
 })
