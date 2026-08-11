0d1d9c7c44c2ad3cab49f0fd0607bc989e443c90
SleepyG11 <makatymba2001@yandex.ru>
2026-08-12 01:43:01 +0300

    more safe nautilus handling

--- content/sg11_n_vekhi/3_fish/nautilus.lua
+++ content/sg11_n_vekhi/3_fish/nautilus.lua
@@ -30,37 +30,39 @@ FishAndChips.Fish({
 					func = function()
 						local w = (G.CARD_W + 0.1) * 1 * 2 - 0.1
 						local h = G.CARD_H
+                        local bait_card
 						G.E_MANAGER:add_event(Event({
-							func = function()
-								G.fac_nautilus_area =
-									CardArea(card.T.x + card.T.w / 2 - w / 2, card.T.y - 0.25 - h, w, h, {
-										type = "joker",
-										card_limit = 1,
-										highlight_limit = 1,
-										highlighted_limit = 1,
-										align_buttons = true,
-										bg_colour = G.C.CLEAR,
-										fixed_limit = true,
-										no_card_count = true,
-									})
+                            func = function()
+                                G.fac_nautilus_area =
+                                    CardArea(card.T.x + card.T.w / 2 - w / 2, card.T.y - 0.25 - h, w, h, {
+                                        type = "joker",
+                                        card_limit = 1,
+                                        highlight_limit = 1,
+                                        highlighted_limit = 1,
+                                        align_buttons = true,
+                                        bg_colour = G.C.CLEAR,
+                                        fixed_limit = true,
+                                        no_card_count = true,
+                                    })
 								return true
 							end,
 						}))
 						G.E_MANAGER:add_event(Event({
 							func = function()
-								local card = SMODS.create_card({ set = "fac_Bait", area = G.fac_nautilus_area })
-								G.fac_nautilus_area:emplace(card)
-								FishAndChips.add_bait_to_inventory(card.config.center.key)
+								local _card = SMODS.create_card({ set = "fac_Bait", area = G.fac_nautilus_area })
+								G.fac_nautilus_area:emplace(_card)
+								FishAndChips.add_bait_to_inventory(_card.config.center.key)
+                                bait_card = _card
 								return true
 							end,
 						}))
 						delay(1.5)
-						G.E_MANAGER:add_event(Event({
-							func = function()
-								G.fac_nautilus_area.cards[1]:start_dissolve()
-								return true
-							end,
-						}))
+                        G.E_MANAGER:add_event(Event({
+                            func = function()
+                                bait_card:start_dissolve()
+                                return true
+                            end,
+                        }))
 						delay(0.5)
 						G.E_MANAGER:add_event(Event({
 							func = function()
