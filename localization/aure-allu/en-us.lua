8c01aaa1a277425f7ff51d7ff25da4d9ca584b41
Casimir Eisenach <casimirusenormus@hotmail.de>
2026-08-11 11:50:36 +0200

    forgor to save that

--- localization/aure-allu/en-us.lua
+++ localization/aure-allu/en-us.lua
@@ -396,7 +396,6 @@ return {
                         "When {C:attention}Blind{} is selected,",
                         "{E:1,C:red}eats{} {C:fac_fish}fish{} to the right",
                         "and gains its {C:attention}weight",
-                        "{s:0.8,C:inactive}(maximum of #4#kg)"
                     },
                     {
                         "This Shark gives {X:mult,C:white}X#1#{} Mult",
