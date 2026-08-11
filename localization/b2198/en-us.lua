0a45d359310251c69c62502500eeeda7fd49cc44
b2198 <onerb98@hotmail.com>
2026-08-11 03:40:49 -0300

    Added first incomplete and bugged state of dimensional greenfishes.

---
+++ localization/b2198/en-us.lua
@@ -0,0 +1,95 @@
+return {
+    descriptions = {
+        fac_Fish = {
+            fish_fac_0dgreenfish = {
+                name = "0D Greenfish",
+                text = {
+                    {
+                        "{C:chips}+#1#{} Chip"
+                    },
+                    {
+                        "Increases in dimensions after",
+                        "defeating {C:attention}#3#{} boss blind"
+                    }
+                },
+                flavor = {
+                    "You have to admit it has a POINT."
+                }
+            },
+            fish_fac_1dgreenfish = {
+                name = "1D Greenfish",
+                text = {
+                    {
+                        "{C:mult}+#1#{} Mult"
+                    },
+                    {
+                        "Increases in dimensions after",
+                        "defeating {C:attention}#3#{} bosses blinds",
+                        "{C:inactive}(Currently {C:attention}#2#{}{C:inactive}){}"
+                    }
+                },
+                flavor = {
+                    "Really helps to aLINE the fishes your bucket."
+                }
+            },
+            fish_fac_2dgreenfish = {
+                name = "2D Greenfish",
+                text = {
+                    {
+                        "{X:mult,C:white}X#1#{} Mult"
+                    },
+                    {
+                        "Increases in dimensions after",
+                        "{C:attention}#3#{} bosses defeated",
+                        "{C:inactive}(Currently {C:attention}#2#{}{C:inactive}){}"
+                    }
+                },
+                flavor = {
+                    "So just like most game fishes, but green?"
+                }
+            },
+            fish_fac_3dgreenfish = {
+                name = "3D Greenfish",
+                text = {
+                    {
+                        "{X:mult,C:white}X#1#{} Mult",
+                        "Increases by {X:mult,C:white}X#2#{} for",
+                        "each owned Fish after",
+                        "defeating a boss blind"
+                    },
+                    {
+                        "Increases in dimensions after",
+                        "{C:attention}#4#{} bosses defeated",
+                        "{C:inactive}(Currently {C:attention}#3#{}{C:inactive}){}"
+                    }
+                },
+                flavor = {
+                    "It's trying its best to be isometric,",
+                    "but was drawn with wrong angles."
+                }
+            },
+            fish_fac_4dgreenfish = {
+                name = "4D Greenfish",
+                text = {
+                    {
+                        "{X:mult,C:white}X#1#{} Mult",
+                        "Increases by {X:mult,C:white}X#2#{} for",
+                        "each owned Fish after",
+                        "defeating a boss blind"
+                    },
+                    {
+                        "Multiplies the scaling of",
+                        "most jokers and fishes by {B:1,C:white}X#3#{}",
+                        -- "After defeating a boss blind,",
+                        -- "multiplies most values on all",
+                        -- "owned fishes by {B:1,C:white}X#2#{}",
+                        -- "{C:inactive}(This effect not included){}"
+                    }
+                },
+                flavor = {
+                    "That's not how fish shapes work in 4D."
+                }
+            }
+        }
+    }
+}
