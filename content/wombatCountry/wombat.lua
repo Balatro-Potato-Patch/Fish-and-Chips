a40e58fd2a5cf5baa846f930dcb07b39189a1455
WilsontheWolf <git@shorty.systems>
2026-08-12 17:14:53 -0600

    Wombat Country Submission

---
+++ content/wombatCountry/wombat.lua
@@ -0,0 +1,401 @@
+SMODS.Atlas {
+	key = "fac_wombat",
+	path = "wombatCountry/wombat.png",
+	px = 71,
+	py = 95
+}
+
+PotatoPatchUtils.Developer({
+	name = 'wombatCountry',
+	atlas = 'fac_wombat',
+	colour = HEX('8AFFBE')
+})
+
+SMODS.Atlas({
+	key = "wombatCountry_fish",
+	path = "wombatCountry/fish.png",
+	px = 71,
+	py = 95,
+})
+
+--the dorphish
+FishAndChips.Fish({
+    key = "wombatCountry_dorphish",
+	weight = 10,
+	atlas = "wombatCountry_fish",
+	pos = { x = 0, y = 0 },
+	ppu_artist = { "wombatCountry" },
+	ppu_coder = { "wombatCountry" },
+	attributes = { "usable", "economy" },
+	environments = {
+		backroom = 1,
+		styx = 0.25,
+        soup = 0.25,
+	},
+	stats = {
+		weight = {min = 6.3, max = 9.9},
+		length = {min = 0.5, max = 1.25}
+	},
+	config = {
+		extra = {
+			dollar_min = 1,
+            dollar_max = 10
+		}
+	},
+	blueprint_compat = false,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.dollar_min, card.ability.extra.dollar_max } }
+	end,
+	use = function(self, card)
+        local payout = pseudorandom('dorphish', card.ability.extra.dollar_min, card.ability.extra.dollar_max)
+		ease_sand_dollars(payout)
+	end,
+	can_use = function(self, card)
+		return true
+	end
+})
+
+--spunched bob
+FishAndChips.Fish({
+    key = "wombatCountry_spunch",
+	weight = 10,
+	atlas = "wombatCountry_fish",
+	pos = { x = 1, y = 0 },
+	ppu_artist = { "wombatCountry" },
+	ppu_coder = { "wombatCountry" },
+	attributes = { "usable", "suit" },
+	environments = {
+		calm_pond = 1,
+		city_river = 0.25,
+	},
+	stats = {
+		weight = {min = 0.014, max = 0.028},
+		length = {min = 0.105, max = 0.12}
+	},
+	config = {
+		extra = {
+			dollar_min = 1,
+            dollar_max = 10
+		}
+	},
+	blueprint_compat = false,
+	use = function(self, card)
+		local suit
+        for _, playing_card in ipairs(G.deck.cards) do
+			suit = pseudorandom_element(SMODS.Suits)
+
+			SMODS.change_base(playing_card, suit.key)
+		end
+		play_sound('timpani')
+	end,
+	can_use = function(self, card)
+		return true
+	end
+})
+
+--sardine boys
+FishAndChips.Fish({
+    key = "wombatCountry_sardine",
+	weight = 10,
+	atlas = "wombatCountry_fish",
+	pos = { x = 2, y = 0 },
+	ppu_artist = { "wombatCountry" },
+	ppu_coder = { "wombatCountry" },
+	attributes = { "usable", "rank" },
+	environments = {
+		pier = 1,
+		city_river = 0.5,
+        soup = 0.5,
+	},
+	stats = {
+		weight = {min = 0.106, max = 0.125},
+		length = {min = 0.1016, max = 0.1143}
+	},
+	config = {
+		extra = {
+			dollar_min = 1,
+            dollar_max = 10
+		}
+	},
+	blueprint_compat = false,
+	use = function(self, card)
+		local rank
+        for _, playing_card in ipairs(G.deck.cards) do
+			rank = pseudorandom_element(SMODS.Ranks)
+
+			SMODS.change_base(playing_card, nil, rank.key)
+		end
+		play_sound('timpani')
+	end,
+	can_use = function(self, card)
+		return true
+	end
+})
+
+--dylan fishmin
+FishAndChips.Fish({
+    key = "wombatCountry_dylan",
+	weight = 10,
+	atlas = "wombatCountry_fish",
+	pos = { x = 0, y = 2 },
+	ppu_artist = { "wombatCountry" },
+	ppu_coder = { "wombatCountry" },
+	attributes = { "mult", "destroy_card", "scaling" },
+	environments = {
+		garden = 1,
+        soup = 0.5,
+	},
+	stats = {
+		weight = {min = 0.3, max = 0.7},
+		length = {min = 0.025, max = 0.035}
+	},
+	config = {
+		extra = {
+			mult = 0.0
+		}
+	},
+	blueprint_compat = true,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.mult} }
+	end,
+	calculate = function(self, card, context)
+		if context.setting_blind and not context.blueprint and #G.fac_fish_area.cards >= 2 and not (G.fac_fish_area.cards[1] == card) then
+			--adds the weight of the leftmost fish to dylan's mult AND weight stat
+			card.ability.extra.mult = card.ability.extra.mult + G.fac_fish_area.cards[1].ability.stats.weight
+			card.ability.stats.weight = card.ability.stats.weight + G.fac_fish_area.cards[1].ability.stats.weight
+
+			SMODS.destroy_cards(G.fac_fish_area.cards[1], nil, nil, true)
+			return {
+				message = localize('fac_wombatCountry_dylan')
+			}
+		end
+		if context.joker_main then
+            return {
+                mult = card.ability.extra.mult,
+            }
+        end
+	end
+})
+
+--yellowfish
+FishAndChips.Fish({
+    key = "wombatCountry_yellow",
+	weight = 10,
+	atlas = "wombatCountry_fish",
+	pos = { x = 1, y = 2 },
+	ppu_artist = { "wombatCountry" },
+	ppu_coder = { "wombatCountry" },
+	attributes = { "xmult", "destroy_card"},
+	environments = {
+		wormhole = 1,
+        pier = 0.25,
+	},
+	stats = {
+		weight = {min = 1.81, max = 4.08},
+		length = {min = 0.127, max = 0.254}
+	},
+	config = {
+		extra = {
+			xmult = 2
+		}
+	},
+	blueprint_compat = true,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.xmult} }
+	end,
+	calculate = function(self, card, context)
+		if context.remove_playing_cards and not context.blueprint then
+			SMODS.destroy_cards(card)
+		end
+		if context.joker_main then
+            return {
+                xmult = card.ability.extra.xmult,
+            }
+        end
+	end
+})
+
+--old boot
+FishAndChips.Fish({
+    key = "wombatCountry_boot",
+	weight = 10,
+	atlas = "wombatCountry_fish",
+	pos = { x = 2, y = 2 },
+	ppu_artist = { "wombatCountry" },
+	ppu_coder = { "wombatCountry" },
+	attributes = { "hand_level", "usable"},
+	environments = {
+		city_river = 1,
+        calm_pond = 0.75,
+		swamp = 0.5,
+	},
+	stats = {
+		weight = {min = 0.8, max = 22.2},
+		length = {min = 0.07, max = 0.825}
+	},
+	blueprint_compat = false,
+	use = function(self, card)
+		-- used some vanilla remade for this one https://github.com/nh6574/VanillaRemade/blob/6ada4a41bca13b7e4b238ade3806c63a2e09dc98/src/jokers.lua#L2068
+		local least_played_hand = G.GAME.hands["High Card"]
+		for hand_name, hand in pairs(G.GAME.hands) do
+			if ((hand.played or 0) < least_played_hand.played) or ((hand.played or 0) == least_played_hand.played and hand.order > least_played_hand.order) and SMODS.is_poker_hand_visible(hand_name) then
+				least_played_hand = hand
+			end
+		end
+		SMODS.upgrade_poker_hands({hands = least_played_hand.key})
+	end,
+	can_use = function(self, card)
+		return true
+	end
+})
+
+--functions to help the fish sticks work
+function fac_wombat_count_sticks (level)
+	local key = "fish_fac_wombatCountry_fishstick_" .. level
+	local count = 0
+	for _, fish in ipairs(G.fac_fish_area.cards) do
+		if fish.config.center_key == key then
+			count = count + 1
+		end
+	end
+	return count
+end
+
+function fac_wombat_merge_sticks (level)
+	local old_key = "fish_fac_wombatCountry_fishstick_" .. level
+	local new_key = "fish_fac_wombatCountry_fishstick_" .. (level + 1)
+	for _, fish in ipairs(G.fac_fish_area.cards) do
+		if fish.config.center_key == old_key then
+			SMODS.destroy_cards(fish, nil, nil, true)
+		end
+	end
+	SMODS.add_card({key = new_key, area = G.fac_fish_area, set = "fac_Fish"})
+end
+
+--one fish stick
+FishAndChips.Fish({
+    key = "wombatCountry_fishstick_1",
+	weight = 10,
+	atlas = "wombatCountry_fish",
+	pos = { x = 0, y = 1 },
+	ppu_artist = { "wombatCountry" },
+	ppu_coder = { "wombatCountry" },
+	attributes = { "chips" },
+	environments = {
+		city_river = 1,
+		chocolate_river = 0.5,
+		soup = 0.125,
+	},
+	stats = {
+		weight = {min = 0.025, max = 0.03},
+		length = {min = 0.07, max = 0.08}
+	},
+	config = {
+		extra = {
+			chips = 20
+		}
+	},
+	blueprint_compat = true,
+	loc_vars = function(self, info_queue, card)
+        info_queue[#info_queue + 1] = G.P_CENTERS.fish_fac_wombatCountry_fishstick_2
+		return { vars = { card.ability.extra.chips} }
+	end,
+	calculate = function(self, card, context)
+		if context.ending_fishing and not context.blueprint then
+			--counts the number of one fish sticks
+			local count = fac_wombat_count_sticks(1)
+			--while there are an even number of one fish sticks, merge them
+			while count % 2 == 0 and count > 0 do
+				fac_wombat_merge_sticks(1)
+				count = count - 2
+			end
+		end
+		if context.joker_main then
+            return {
+                chips = card.ability.extra.chips,
+            }
+        end
+	end
+})
+
+--two fish sticks
+FishAndChips.Fish({
+    key = "wombatCountry_fishstick_2",
+	weight = 4,
+	atlas = "wombatCountry_fish",
+	pos = { x = 1, y = 1 },
+	ppu_artist = { "wombatCountry" },
+	ppu_coder = { "wombatCountry" },
+	attributes = { "chips" },
+	environments = {
+		city_river = 1,
+		chocolate_river = 0.5,
+		soup = 0.125,
+	},
+	stats = {
+		weight = {min = 0.025, max = 0.03},
+		length = {min = 0.07, max = 0.08}
+	},
+	config = {
+		extra = {
+			chips = 60
+		}
+	},
+	blueprint_compat = true,
+	loc_vars = function(self, info_queue, card)
+        info_queue[#info_queue + 1] = G.P_CENTERS.fish_fac_wombatCountry_fishstick_3
+		return { vars = { card.ability.extra.chips} }
+	end,
+	calculate = function(self, card, context)
+		if context.ending_fishing and not context.blueprint then
+			--counts the number of two fish sticks
+			local count = fac_wombat_count_sticks(2)
+			--while there are an even number of two fish sticks, merge them
+			while count % 2 == 0 and count > 0 do
+				fac_wombat_merge_sticks(2)
+				count = count - 2
+			end
+		end
+		if context.joker_main then
+            return {
+                chips = card.ability.extra.chips,
+            }
+        end
+	end
+})
+
+--three fish sticks
+FishAndChips.Fish({
+    key = "wombatCountry_fishstick_3",
+	weight = 1,
+	atlas = "wombatCountry_fish",
+	pos = { x = 2, y = 1 },
+	ppu_artist = { "wombatCountry" },
+	ppu_coder = { "wombatCountry" },
+	attributes = { "chips" },
+	environments = {
+		city_river = 1,
+		chocolate_river = 0.5,
+		soup = 0.125,
+	},
+	stats = {
+		weight = {min = 0.025, max = 0.03},
+		length = {min = 0.07, max = 0.08}
+	},
+	config = {
+		extra = {
+			chips = 180
+		}
+	},
+	blueprint_compat = true,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.chips} }
+	end,
+	calculate = function(self, card, context)
+		if context.joker_main then
+            return {
+                chips = card.ability.extra.chips,
+            }
+        end
+	end
+})
---
+++ localization/wombatCountry/en-us.lua
@@ -0,0 +1,127 @@
+return {
+    descriptions = {
+		PotatoPatch = {
+			PotatoPatchDev_wombatCountry = {
+				name = "Wombat Country",
+			},
+        },
+        fac_Fish = {
+            fish_fac_wombatCountry_dorphish = {
+                name = "The Dorphish",
+                text = {
+                    "earn between",
+                    "{C:fac_sand_dollars,f:fac_sand_dollars}${C:fac_sand_dollars}#1#-#2#{}",
+                    "Sand dollars",
+                    "{ppu_bubble:usable}"
+                },
+                flavor = {
+                    "Stand and unfold yourself!",
+                    "Long live the king!",
+                    "-William Shakespeare"
+                }
+            },
+            fish_fac_wombatCountry_dylan = {
+                name = "Dylan Fishmin",
+                text = {
+                    "At the start of the",
+                    "round, {C:attention}eat{} the {C:attention}leftmost{}",
+                    "fish and stores its",
+                    "weight as {C:mult}mult{}",
+                    "{C:inactive}(currently {C:mult}+#1#{}{C:inactive} mult){}"
+                },
+                flavor = {
+                    "\"I eat fishmin, like fishmin eat algae!\""
+                }
+            },
+            fish_fac_wombatCountry_fishstick_1 = {
+                name = "One Fish Stick",
+                text = {
+                    "{C:chips}+#1# chips.",
+                    "If you have two after",
+                    "fishing, then merge",
+                    "them into {C:attention}Two Fish Sticks{}"
+                },
+                flavor = {
+                    "Now, correct me if I'm wrong,",
+                    "but I THINK, this is a Fish Stick."
+                }
+            },
+            fish_fac_wombatCountry_fishstick_2 = {
+                name = "Two Fish Sticks",
+                text = {
+                    "{C:chips}+#1# chips.",
+                    "If you have two after",
+                    "fishing, then merge",
+                    "them into {C:attention}Three Fish Sticks{}"
+                },
+                flavor = {
+                    "holy shit two fish sticks"
+                }
+            },
+            fish_fac_wombatCountry_fishstick_3 = {
+                name = "Three Fish Sticks",
+                text = {
+                    "{C:chips}+#1# chips",
+                },
+                flavor = {
+                    "Upon this fine day, the gods doth",
+                    "gift thee fabulous secret powers!"
+                }
+            },
+            fish_fac_wombatCountry_spunch = {
+                name = "Spunched Bob",
+                text = {
+                    "Randomizes the",
+                    "{C:attention}suit{} of all cards",
+                    "in full deck",
+                    "{ppu_bubble:usable}"
+                },
+                flavor = {
+                    "Deypuhspunjbabindufihumah"
+                }
+            },
+            fish_fac_wombatCountry_sardine = {
+                name = "Sardine Boys",
+                text = {
+                    "Randomizes the",
+                    "{C:attention}rank{} of all cards",
+                    "in full deck",
+                    "{ppu_bubble:usable}"
+                },
+                flavor = {
+                    "Everybody's gone fishin'",
+                    "Fishin' U.S.A."
+                }
+            },
+            fish_fac_wombatCountry_yellow = {
+                name = "Yellowfish",
+                text = {
+                    "{X:mult,C:white,}x#1#{} mult.",
+                    "{C:red}self destructs{} when a",
+                    "{C:attention}playing card{} is destroyed"
+                },
+                flavor = {
+                    "A Vessel of Justice for the",
+                    "Depths of the UnderWater"
+                }
+            },
+            fish_fac_wombatCountry_boot = {
+                name = "old boot lol",
+                text = {
+                    "Level up your",
+                    "{C:attention}least played hand{}",
+                    "{C:inactive(weakest if tie){}",
+                    "{ppu_bubble:usable}"
+                },
+                flavor = {
+                    "A supremely unfortunate find."
+                }
+            },
+        }
+    },
+    misc = {
+        dictionary = {
+            fac_wombatCountry_dylan = 'Yummy!',
+        }
+    }
+}
