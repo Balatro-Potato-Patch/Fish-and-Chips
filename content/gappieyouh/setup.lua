c3c2ba199dce2f2424e219d171a96ba77c18ae33
Youh <lipegrs+5@gmail.com>
2026-08-12 07:00:31 -0300

    changed setup.lua

--- content/gappieyouh/setup.lua
+++ content/gappieyouh/setup.lua
@@ -1,13 +1,15 @@
 PotatoPatchUtils.Developer({
 	name = 'Gappie',
-	atlas = 'prototypes',
+	atlas = 'gy_icons',
 	colour = HEX('FFCB00FF'),
+	pos = {x=0,y=0},
 	fac_partner = 'fac_Youh',
 })
 PotatoPatchUtils.Developer({
 	name = 'Youh',
-	atlas = 'prototypes',
+	atlas = 'gy_icons',
 	colour = G.C.CHIPS,
+	pos = {x=1,y=0},
 	fac_partner = 'fac_Gappie',
 })
 SMODS.Atlas {
@@ -15,4 +17,10 @@ SMODS.Atlas {
     path = 'gappieyouh/GappieYouhFish.png',
     px = 71,
     py = 95
+}
+SMODS.Atlas {
+	key = "gy_icons",
+	path = 'gappieyouh/gy_icons.png',
+	px = 71,
+	py = 95
 }
