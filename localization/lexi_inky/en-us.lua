0d782555a56478d3ae9f526b573092cfc69574c3
WilsontheWolf <git@shorty.systems>
2026-08-12 17:24:01 -0600

    Lexi and Inky Submission

---
+++ localization/lexi_inky/en-us.lua
@@ -0,0 +1,204 @@
+return {
+	descriptions = {
+		PotatoPatch = {
+			PotatoPatchDev_lexi = {
+				name = "lexi",
+				text = {
+					{
+						"I pretend to know how to code",
+					},
+					{
+						"click me to open my carrd page",
+					},
+					{
+						"shoutout bluu! (shes the goat)",
+					},
+				},
+			},
+			PotatoPatchDev_inky = {
+				name = "Inky",
+				text = {
+					{
+						"Hi, I'm inky. and i pretend to know how to draw.",
+					},
+					{
+						"click me to open my kofi to commission me",
+					},
+				},
+			},
+		},
+		fac_Fish = {
+			fish_fac_l_i_id = {
+				name = "Accountant's ID",
+				text = {
+					"Gives {C:fac_sand_dollars,f:fac_sand_dollars}${C:fac_sand_dollars}#1#{} for each {C:fac_fish}Fish",
+					"with the {C:money}Economy{} attribute",
+					"at the end of the round",
+				},
+				flavour = {
+					"Living once in wages,",
+					"nows stays taped to your pages",
+				},
+			},
+			fish_fac_l_i_feather = {
+				name = "Black Feather",
+				text = {
+					"All played cards are",
+					"converted to {C:hearts}Hearts{}",
+				},
+				flavour = {
+					"It's strangely cold to the touch",
+					"and smells like blood.",
+				},
+			},
+			fish_fac_l_i_square = {
+				name = "Square Fish",
+				text = {
+					"Played {C:attention}4s{} give",
+					"{C:fac_sand_dollars,f:fac_sand_dollars}${C:fac_sand_dollars}#1#{} when scored",
+				},
+				flavour = {
+					"{s:1.1}(Quadrilateret rightang)",
+					"Believed by little to be the",
+					"best source of protein in pairs",
+				},
+			},
+			fish_fac_l_i_wood = {
+				name = "Wooden #1# Fish",
+				text = {
+					"{C:attention}Aces{}, {C:attention}3s{}, and {C:attention}7s",
+					"gain {C:mult}+#2#{} Mult",
+					"when scored",
+				},
+				flavour = {
+					"{s:1.1}(Palacium pro plankei)",
+				},
+			},
+			fish_fac_l_i_plastic = {
+				name = "Plastic #1# Fish",
+				text = {
+					"{C:attention}4s{} and {C:attention}8s{} give",
+					"{C:purple}+#2#{} Score when scored",
+				},
+				flavour = {
+					"{s:1.1}(Palacium pro plastei)",
+				},
+			},
+			fish_fac_l_i_phish_bait = {
+				name = "Phish Bait",
+				text = {
+					"{X:mult,C:white}X999{} Mult",
+				},
+				flavour = {
+					"{s:1.1}(Espem clikbeid)",
+					"{E:1,C:edition}INCREDIBLE FISH ABILITIES,",
+					"{E:1,C:edition}ALL AT AN ALL-TIME LOW COST!",
+					"{E:1,C:edition}HURRY TO FIND OUT!",
+				},
+			},
+			fish_fac_l_i_phish_bait_real = {
+				name = "Phish Bait",
+				text = {
+					"{X:mult,C:white}X3{} Mult",
+					"debuffs adjacent {C:fac_Fish}Fish",
+					"when a hand is played",
+				},
+				flavour = {
+					"{s:1.1}(Espem clikbeid)",
+					"{E:1,C:edition}INCREDIBLE FISH ABILITIES,",
+					"{E:1,C:edition}ALL AT AN ALL-TIME LOW COST!",
+					"{E:1,C:edition}HURRY TO FIND OUT!",
+				},
+			},
+			fish_fac_l_i_yhsifishy = {
+				name = "yhsiFishy",
+				text = {
+					"If played hand is {C:attention}5{} cards",
+					"and a palindrome, gain {C:fac_sand_dollars,f:fac_sand_dollars}${C:fac_sand_dollars}#1#{}",
+					"{C:inactive}(Eg: 35653)",
+				},
+				flavour = {
+					"{s:1.1}(Reverseum muesrever)",
+					"Do geese see god?",
+				},
+			},
+			fish_fac_l_i_freddy = {
+				name = "Freddy Fishbear",
+				text = {
+					"If played hand is exactly {C:attention}4{} scoring cards",
+					"change their ranks to",
+					"{C:attention}Ace{}, {C:attention}9{}, {C:attention}8{}, and {C:attention}7{} respective to",
+					"their position in your hand",
+				},
+				flavour = {
+					"{s:1.1}(Phiv knight freedee)",
+					"Tastes similarly to pizza..",
+				},
+			},
+			fish_fac_l_i_kevin = {
+				name = "Kevin",
+				text = {
+					"If chips are less than {C:attention}#2#%",
+					"of required chips {X:mult,C:white}X#1#{} Mult",
+				},
+				flavour = {
+					"{s:1.1}(Kevus immortali)",
+					"This could been sent to your local museum,",
+					"but it insists to help you on your run.",
+				},
+			},
+			fish_fac_l_i_fof = {
+				name = "Filet-O-Fish",
+				text = {
+					"Eat this {C:fac_fish}Burger{} to gain",
+					"{C:red}+#1#{} temporary Discards",
+					"{C:inactive,s:0.8}Only useable while",
+					"{C:inactive,s:0.8}a blind is selected",
+					"{ppu_bubble:usable}",
+				},
+				flavour = {
+					"{s:1.1}(Mackee dues)",
+					"only $3.79",
+				},
+			},
+			fish_fac_l_i_fish_ost = {
+				name = "Fish Tycoon OST",
+				text = {
+					"NCS music btw",
+					"{X:mult,C:white}X#1#{} Mult",
+					"{C:money}$#2#",
+					"{C:inactive}it sounds like shit on purpose",
+				},
+				flavour = {
+					"{s:1.1}(Disckus roablots)",
+					"Reminds me of 2016 tycoons",
+				},
+			},
+			fish_fac_l_i_fishsocks = {
+				name = "Fishsocks",
+				text = {
+					"If played hand is a {C:attention}#1#",
+					"Create an editioned {C:fac_fish}Fish",
+					"{C:inactive}(Negative excluded)",
+					"{C:inactive}(Must have room)",
+				},
+				flavour = {
+					"{s:1.1}(Reamainds mioherr)",
+					"...but in the end,",
+					"all we wanted to obtain from here",
+					"were some stupid fish socks...",
+					"Everything reminds me of her.",
+				},
+			},
+		},
+	},
+	misc = {
+		dictionary = {
+			k_fac_l_i_id = "ID Card",
+			k_fac_l_i_feather = "Feather",
+			k_fac_l_i_burger = "Burger",
+			k_fac_l_i_music = "Music",
+			b_fac_l_i_eat = "Eat",
+		},
+	},
+}
