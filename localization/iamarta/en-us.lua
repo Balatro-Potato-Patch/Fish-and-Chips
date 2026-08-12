d446e6b6b2927d5a8615b7cad868a645a6b1b165
iamarta <arta.mz1111@gmail.com>
2026-08-11 22:32:39 -0400

    All my stuff at once because I forgot to commit consistently

---
+++ localization/iamarta/en-us.lua
@@ -0,0 +1,121 @@
+return {
+    descriptions = {
+        PotatoPatch = {
+            PotatoPatchDev_iamarta = {
+                name = "iamarta",
+                text = {
+                    "bonjour again!",
+                    "if you're ever wondering why the art on",
+                    "my fish is so bad, that's because i",
+                    "made it.",
+                    "{C:dark_edition,E:1}I HOPE YOU LIKE MY STUFF!!!"
+                }
+            }
+        },
+        fac_Fish = {
+            fish_fac_iamarta_100_gar = {
+                name = "Like 100 Gar",
+                text = {
+                    "If a catch is {C:attention,E:1}Perfect{},",
+                    "this Fish eats one of your",
+                    "{C:attention}baits{} and gains {X:mult,C:white}X#1#{} Mult",
+                    "{C:inactive}(Currently {X:mult,C:white}X#2#{C:inactive} Mult)"
+                },
+                flavour = {
+                    "Or like, 100 gar would be bad too..."
+                }
+            },
+            fish_fac_iamarta_memory_fish = {
+                name = "Memory Fish",
+                text = {
+                    "Earn {C:money}$#1#{} when a Fish is",
+                    "caught in the {C:attention}Environment{}",
+                    "this Fish was caught in",
+                    "{C:inactive}(#2#)"
+                },
+                flavor = {
+                    "*Squuuuuiiiiissshhhh*"
+                }
+            },
+            fish_fac_iamarta_fish_xing = {
+                name = "Fish Xing",
+                text = {
+                    "Use to destroy up to",
+                    "{C:attention}#1#{} selected card(s),",
+                    "gains {C:attention}+#2#{} when a",
+                    "fishing attempt {C:red}fails"
+                },
+                flavor = {
+                    "How do fish know to",
+                    "cross at the sign?"
+                }
+            },
+            fish_fac_iamarta_punch_carp = {
+                name = "Punch Carp",
+                text = {
+                    "Makes the {C:attention}Fish{} to the right",
+                    "guaranteed to be the next",
+                    "reeled fish, but makes",
+                    "it {C:red}very hard{} to catch.",
+                    "{C:red,E:2}self destructs",
+                    "{C:inactive}(regardless of success)"
+                },
+                flavour = {
+                    "Can you believe they used to code",
+                    "fish on a card?"
+                }
+            },
+            fish_fac_iamarta_hot_dogfish = {
+                name = "Hot Dogfish",
+                text = {
+                    "{C:attention}Use{} for {X:mult,C:white}X#1#{} Mult until",
+                    "end of round",
+                    "{C:inactive}(#2# Uses Remaining)"
+                },
+                flavour = {
+                    "I prefer mine with mustard"
+                }
+            },
+            fish_fac_iamarta_glider = {
+                name = "Glider",
+                text = {
+                    "If a played card has 2",
+                    "{C:attention}neighbours{} with higher ranks,",
+                    "its rank will {C:attention}increase"
+                },
+                flavour = {
+                    'Also known as a "Fish"'
+                }
+            },
+            fish_fac_iamarta_big_fish = {
+                name = "Big Fish",
+                text = {
+                    "{X:blind,C:white}X#1#{} Blind Size",
+                    "After {C:attention}#2#{} rounds,",
+                    "{C:attention}use{} for {C:attention}-#3#{} Ante",
+                    "{C:inactive}(Currently #4#/#2#)"
+                },
+                flavour = {
+                    "Holy friendslop am I right?"
+                }
+            },
+            fish_fac_iamarta_big_worm = {
+                name = "Big Worm",
+                text = {
+                    "Creates a {C:fac_sand_dollars}Classic Canned{} after",
+                    "{C:attention}every other{} fishing attempt",
+                    "{C:inactive}(Doesn't trigger on perfect catch)"
+                },
+                flavour = {
+                    "This is a really good reference you gotta believe me"
+                }
+            }
+        }
+    },
+    misc = {
+        dictionary = {
+            k_fac_iamarta_inactive_ex = "Inactive!",
+            k_fac_iamarta_plus_bait = "+1 Bait"
+        }
+    }
+}
