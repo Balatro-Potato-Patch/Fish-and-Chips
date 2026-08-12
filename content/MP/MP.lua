072c2c796ad764c363ea2270ac6c1c1c6e13b0b3
Carrier4133 <50059895+Carrier4133@users.noreply.github.com>
2026-08-12 16:33:29 -0700

    Update MP.lua

--- content/MP/MP.lua
+++ content/MP/MP.lua
@@ -1,8 +1,7 @@
 PotatoPatchUtils.Developer({
 	name = 'Carrier4133 (MP)',
-	atlas = 'MP/fac_MPcards.png', --reused from Wormhole because I'm tired (credit art by SarcPot)
-	pos = { x = 0, y = 0 }
-	colour = G.C.BLACK,
+	atlas = 'fac_MPcards',
+	colour = G.C.YELLOW,
 	ignore_limits = false, -- USING THIS VALUE WILL RESULT IN YOUR SUBMISSION BEING REJECTED
 })
 
