c63331fcbc83728cde94c1f7848e97b649ca13b8
WilsontheWolf <git@shorty.systems>
2026-08-12 17:24:31 -0600

    marchii & ivy's submission

---
+++ localization/marshii-ivy/en-us.lua
@@ -0,0 +1,117 @@
+return {
+    descriptions = {
+		PotatoPatch = {
+			PotatoPatchDev_ivy = {
+				name = "Ivy (and May)",
+                text = {
+                    {
+                        "hi, we're {C:fac_ivy_orange}ivy{} & {C:fac_may_pink}May!{} ..mostly ivy though!",
+                        "{C:fac_felli}we{} had.. quite a lot going on mentally during F&C",
+                        "but {C:fac_felli}we{} tried {C:fac_felli}our{} best!",
+                        "follow my tumblr {C:fac_ivy_orange}-ivy{}",
+                        "take care of yourself {C:fac_may_pink}-May{}"
+                    },
+                }
+			},
+            PotatoPatchDev_marshii = {
+                name = "MarshiiRose",
+                text = {
+                    "Hihi! I'm {C:fac_marshii_colour}MarshiiRose{}, and this is my {C:attention,E:1}first time{} in a modding event :3",
+                    "I'm so happy I got to do this, and I hope I continue to improve <3",
+                    "{C:inactive}(even though I highkey slept through the whole thing)",
+                    "And if you aren't {C:red}allergic{} to them, I've also {C:dark_edition}blessed{} you with {C:uncommon,E:1}pistachios!!",
+                    '{C:inactive}"*yawn* Ill just start tomorr- THE DEADLINE IS WHEN??" -MarshiiRose 11 Aug, 2026'
+                }
+            }
+        },
+        fac_Fish = {
+            -- fish_fac_cod = {
+            --     name = "Common Cod",
+            --     text = {
+            --         "{C:chips}+#1#{} Chips"
+            --     },
+            --     flavor = {
+            --         "This fish likes to",
+            --         "call itself Zubin."
+            --     }
+            -- },
+            fish_fac_stencil = {
+                name = "Fish Stencil",
+                text = {
+                    "{X:mult,C:white}X#1#{} Mult for every",
+                    "empty {C:attention}Bucket{} space",
+                    "{C:inactive}(Currently {X:mult,C:white}X#2#{C:inactive} Mult)"
+                },
+                flavor = {
+                    "It's just a piece of paper, but",
+                    "someone cut out a fish-like shape."
+                }
+            },
+            fish_fac_tsnefish = {
+                name = "This is not a fish.",
+                text = {
+                    "Use this {C:blue}Fish{} to transform another",
+                    "{C:blue}Fish{} into a random {C:attention}Joker{}",
+                    "{C:inactive}(May be used once per round)"
+                },
+                flavor = {
+                    "The famous fish. How people reproached me for it!",
+                    "And yet, could you fish up my fish?",
+                    "No, it's just a representation, is it not?",
+                    "So if I had written on my picture \"This is a fish\",",
+                    "I'd have been lying!"
+                }
+            },
+            fish_fac_thefuckingabstractone = {
+                name = "The Abstract One",
+                text = {
+                    "After {C:blue}Fishing{}, this {C:blue}Fish{}",
+                    "gains {C:mult}+#1#{} Mult if you succeeded",
+                    "and loses {C:mult}#2#{} Mult otherwise",
+                    "{C:inactive}(Currently {C:mult}+#3#{C:inactive} Mult)"
+                },
+                flavor = {
+                    "It's not a fish, it's not!",
+                    "but it *captures* the *essence* of",
+                    "what a \"Fish\" truly is!"
+                }
+            },
+            fish_fac_fishbone_dagger = {
+                name = "Fishbone Dagger",
+                text = {
+                    "When {C:attention}Blind{} selected,",
+                    "destroy the {C:blue}Fish{} to the",
+                    "right, and add {C:attention}ten times{} its",
+                    "sell value to this {C:blue}Fish's{} Chips",
+                    "{C:inactive}(Currently {C:chips}+#1#{C:inactive} Chips)"
+                },
+                flavor = {
+                    "It's not technically alive, but",
+                    "that doesn't mean you should let",
+                    "your kids play with this thing, it's sharp!"
+                },
+            },
+        },
+    },
+    misc = {
+        achievement_descriptions = {},
+        achievement_names = {},
+        blind_states = {},
+        challenge_names = {},
+        collabs = {},
+        dictionary = {
+            k_ivy_not_a_fish = "Not a Fish"
+        },
+        high_scores = {},
+        labels = {},
+        poker_hand_descriptions = {},
+        poker_hands = {},
+        quips = {},
+        ranks = {},
+        suits_plural = {},
+        suits_singular = {},
+        tutorial = {},
+        v_dictionary = {},
+        v_text = {},
+    },
+}
