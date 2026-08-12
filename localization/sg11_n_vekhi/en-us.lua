2ab0d07e2c443beb4edf679b898211f8b7a21304
SleepyG11 <makatymba2001@yandex.ru>
2026-08-12 04:20:36 +0300

    make localization not crash, actually xd

--- localization/sg11_n_vekhi/en-us.lua
+++ localization/sg11_n_vekhi/en-us.lua
@@ -186,7 +186,7 @@ return {
                     "{C:attention}Enhancement{} to all cards held in hand",
                 },
                 flavour = {
-                    "Gomen, Amanai..."
+                    "Gomen, Amanai...",
                     "{element:1}",
                 },
             },
@@ -215,7 +215,7 @@ return {
     misc = {
         dictionary = {
             pac_quantum_fish_1 = "You again...",
-            pac_quantum_fish_2 = "I'll not let you go this time.",
+            pac_quantum_fish_2 = "I will not let you go this time.",
             pac_quantum_fish_3 = "Say 'Fish'!",
             k_pac_soul_cannon_trigger = "Soul Cannon Activated",
             k_fac_recycle_ex = "Recycle!",
