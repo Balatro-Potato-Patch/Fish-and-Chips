34566ba732dc7b472226eb63bea7c5e9f0971a01
FurretWalk <127532786+FurretWalk@users.noreply.github.com>
2026-08-12 20:03:40 +0700

    h
    Is this how it works or what

---
+++ localization/FurretWalk/en-us.lua
@@ -0,0 +1,50 @@
+return { 
+    descriptions = {
+    PotatoPatch = {
+			PotatoPatchDev_FurretWalk = {
+				name = "FurretWalk",
+                text = {
+                    "{C:tarot}Oh, bait",
+                    "{C:mult}To chomp upon this {C:money}bait",
+                    "{C:green}To chomp upon the {C:attention}glow of the bright {C:tarot}lure",
+                    "{C:tarot}It fits me nicely, {C:mult}though",
+                    "{C:mult}It fits me like the {C:money}depths below",
+                    "{C:money}To {C:green}be",
+                    "{C:attention}Exactly only {C:tarot}me",
+                    "{C:mult}The fish that i was {C:money}always meant to {C:green}be",
+                    "{C:green}All going swimming{C:attention}ly",
+                    "{C:attention}I dive into this {C:tarot}life freely",
+                    "{C:inactive}-Nekomura Iroha (?), \"streetcatfish\"",
+                    "{C:inactive}(Play SynthB and Walkies!)"
+
+                }
+			},
+        },
+        fac_Fish = {
+            fish_fac_miketrout = {
+                name = "Mike Trout",
+                text = {
+                    "{C:fish}Fish{} give {X:mult,C:white}X#1#{} Mult,",
+                    "gains {X:mult,C:white}X#2#{} after Blind is defeated",
+                },
+                flavor = {
+                    "This fish can stay for",
+                    "15 years, even in the",
+                    "worst situations!"
+                }
+            },
+            fish_fac_fishmael = {
+                name = "Fishmael",
+                text = {
+                    "Destroys scoring {C:hearts}#3#{} cards and",
+                    "gains {C:chips}+#2#{} Chips for every {C:hearts}Heart{} destroyed",
+                    "{C:inactive}(Currently {C:chips}+#1#{} Chips)"
+                },
+                flavor = {
+                    "This fish is piercing through the rope",
+                    "that strangled you and me...",
+                }
+            },
+        }
+}
+}
