08579c09ba761eb8d1292c27b27e647fe46691e3
Carrier4133 <50059895+Carrier4133@users.noreply.github.com>
2026-08-12 08:21:17 -0700

    Loc update.

---
+++ localization/MP/en-us.lua
@@ -0,0 +1,73 @@
+return {
+    descriptions = {
+        fac_Fish = {
+            fish_fac_test = {
+                name = "Carpboard Cutout",
+                text = {
+                    "Placeholder"
+                }
+            },
+            letter_fish = {
+                name = "Letter Fish",
+                text = {
+                    "A humble fish that sends mail."
+                }
+            },
+            luck_statue = {
+                name = "Luck Statue",
+                text = {
+                    "Looks like Ehekatl took your fish. Have a statue instead."
+                }
+            },
+            toxikarp = {
+                name = "Toxikarp",
+                text = {
+                    "Its corrosive bubbles melt through blinds and fish alike."
+                }
+            },
+            size_2 = {
+                name = "Size Two",
+                text = {
+                    "So the rumors were true after all."
+                }
+            },
+
+            gezora = {
+                name = "Gezora",
+                text = {
+                    "This ISN'T Not-Gezora, by the way."
+                }
+            },
+            crystal_serpent = {
+                name = "Crystal Serpent",
+                text = {
+                    "The scales shimmer in the light."
+                }
+            },
+            reaver_fish = {
+                name = "Reaver Shark",
+                text = {
+                    "Used to mine ore in the depths of Hell."
+                }
+            },
+            halibut_cannon = {
+                name = "Halibut Cannon",
+                text = {
+                    "Yup. Still overpowered."
+                }
+            },
+            trash_crab = {
+                name = "Trash Crab",
+                text = {
+                    "Collects bits of junk and rewards you slowly."
+                }
+            },
+            primordial_wyrm = {
+                name = "Primordial Wyrm",
+                text = {
+                    "A colossal wyrm that bends the blind."
+                }
+            },
+        },
+    }
+}
