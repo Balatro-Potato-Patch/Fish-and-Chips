5951aec9513db3c93870d72ed0b3521e1cb1c3e2
WilsontheWolf <git@shorty.systems>
2026-08-12 17:22:56 -0600

    Astra & Missingno Submission

---
+++ localization/astra-missingno/en-us.lua
@@ -0,0 +1,220 @@
+return {
+    descriptions = {
+		PotatoPatch = {
+			PotatoPatchDev_theAstra = {
+				name = "theAstra",
+                text = {
+                    {"Howdy! I'm {C:purple,E:2}Astra{}, head of the {C:attention}Potato Patch!",
+                    "Thanks for playing the mod! This was easily our {C:red}most ambitious",
+                    "event mod yet, and I had a blast making it happen! Shout out to",
+                    "my fellow {C:attention}Organizers{} and the {C:planet}Guest Dev{} team for bringing this crazy",
+                    "vision to life!",
+                    "{C:dark_edition,s:1.4}To The Stars!!! {C:dark_edition,s:1.4,f:8}🪐"},
+                    {"Also check out this Brooke Trout I caught irl",
+                    "during the development of this mod",
+                    "{element:1}"}
+                }
+			},
+			PotatoPatchDev_MissingNo = {
+				name = "MissingNo",
+                text = {
+                    "{C:inactive,s:0.8}#1#",
+                    "Artist for {C:red}0 ERROR{} and {C:purple}Finity",
+                    "unhinged internet idiot",
+                    "Listen to {C:blue,u:blue}my music{} to die instantly"
+                }
+			},
+        },
+        fac_Fish = {
+            fish_fac_am_jerry = {
+                name = "Jerry",
+                text = {
+                    "Gives {C:chips}Chips{} equal",
+                    "to {C:attention}x#1#{} the current",
+                    "{C:attention}Music Volume{} setting",
+                    "{C:inactive,s:0.8}Currently: {s:0.8,C:chips}+#2# {C:inactive,s:0.8}Chips"
+                },
+                flavor = {
+                    "This music-loving jellyfish",
+                    "is the lead bassist for",
+                    "the Wave Breakers!"
+                }
+            },
+            fish_fac_am_king = {
+                name = "King of the Pond",
+                text = {
+                    "{X:red,C:white}X#1#{} Mult, gains {X:red,C:white}X#2#{} Mult for",
+                    "each played and scoring {C:attention}King{}",
+                },
+                flavor = {
+                    "There can only be one",
+                    "true king",
+                }
+            },
+            fish_fac_am_missingno = {
+                name = "MissingNo.",
+                text = {
+                    "Next opened {C:attention}Booster Pack",
+                    "will contain a random"
+                },
+                flavor = {
+                    "{X:green,C:white}BIRD{} {X:inactive,C:white}NORMAL{}",
+                    " ",
+                    " ",
+                }
+            },
+            fish_fac_am_shrimp = {
+                name = "Shrimp Scamperer",
+                text = {
+                    "{C:mult}+#1#{} Mult, gains {C:mult}+#2#{} Mult if a",
+                    " hand type is played {C:attention}#3#{C:inactive}[#4#]{} times",
+                    "{C:inactive,s:0.8}Current hand: #5#"
+                },
+                flavor = {
+                    "One! Two! Three!",
+                    "Three! Two! One!",
+                }
+            },
+            fish_fac_am_ascii = {
+                name = "Ascii Fish",
+                text = {
+                    "{C:attention}Retriggers{} played cards",
+                    "with non-number ranks",
+                    "{C:attention}#1#{} time"
+                },
+                flavor = {
+                    " _",
+                    "><_>"
+                }
+            },
+            fish_fac_am_teabag = {
+                name = "Teabag",
+                text = {
+                    "Gives {C:attention}1",
+                    "selected card ",
+                    "permanent {C:money}$#1#{}",
+                    "when scored",
+                    "{ppu_bubble:usable}"
+                },
+                flavor = {
+                    "Finding one of these in",
+                    "the harbour must mean",
+                    "you're in Boston"
+                }
+            },
+            fish_fac_am_starcatcher = {
+                name = "Starcatcher",
+                text = {
+                    "Before scoring, {C:red}eats",
+                    "all scoring {C:diamonds}Diamond{} cards",
+                    "and gains {C:fac_sand_dollars,f:fac_sand_dollars}$#1#{} of sell value",
+                    "for each eaten {C:diamonds}Diamond{}"
+                },
+                flavor = {
+                    "Its tentacles dangle with",
+                    "anticipation of a meal",
+                    "floating by"
+                }
+            },
+            fish_fac_am_chameleon = {
+                name = "Chameleon",
+                text = {
+                    "Copies the",
+                    "{C:attention}centermost{} Joker",
+                    "{C:inactive,s:0.8}Must have odd number of Jokers"
+                },
+                flavor = {
+                    "It attempts to blend into",
+                    "anything and everything,",
+                    "although usually quite poorly"
+                }
+            },
+            fish_fac_am_mola = {
+                name = "Mola Mola",
+                text = {
+                    {"Disables effect of",
+                    "every {C:attention}Boss Blind",},
+                    {"{C:green}#1# in #2#{} chance to {C:red,E:1}die{}",
+                    "at the end of the {C:attention}shop{}"}
+                },
+                flavor = {
+                    "While it's great at",
+                    "deterring parasites,",
+                    "it's quite terrible at",
+                    "literally everything else"
+                }
+            },
+            fish_fac_am_dopefish = {
+                name = "Dopefish",
+                text = {
+                    "When blind is selected,",
+                    "this {C:fac_fish}Fish {C:red,E:1}eats{} all",
+                    "{C:attention}adjacent {C:fac_fish}Fish{} and gains",
+                    "{X:red,C:white}X#2#{} for each {C:fac_fish}Fish{} eaten",
+                    "{C:inactive,s:0.8}Currently: {X:red,C:white,s:0.8}X#1#{C:inactive,s:0.8} Mult"
+                },
+                flavor = {
+                    "The second dumbest",
+                    "creature in the universe,",
+                    "they'll eat anything alive",
+                    "and moving near them"
+                }
+            },
+            fish_fac_am_piscis = {
+                name = "Piscis Austrinus",
+                text = {
+                    {"Use this {C:fac_fish}Fish{} to upgrade",
+                    "the level of the {C:attention}last played{}",
+                    "poker hand #1# times",
+                    "{ppu_bubble:usable}"},
+                    {"{S:0.8}({S:0.8,V:1}lvl.#2#{S:0.8}){} Level up",
+                    "{C:attention}#3#",
+                    "{C:mult}+#4#{} Mult and",
+                    "{C:chips}+#5#{} chips",}
+                },
+                flavor = {
+                    "It is said that gazing at the",
+                    "dazzling scales of this fish",
+                    "gives you a glimpse into",
+                    "another universe"
+                }
+            },
+            fish_fac_am_blubby = {
+                name = "Blubby",
+                text = {
+                    "{C:attention}1{} free {C:green}Reroll",
+                    "for every {C:attention}#1#%{} of score",
+                    "overshot each round",
+                    "{C:inactive,s:0.8}Currently: {C:green,s:0.8}#2# Rerolls",
+                },
+                flavor = {
+                    "It seems like this Fish came",
+                    "from a factory somewhere...",
+                    "Who knows what wonders",
+                    "are made there"
+                }
+            },
+            fish_fac_am_chocolat = {
+                name = {"Le Fishe", "au chocolat"},
+                text = {
+                    "Applies a {C:green}random{} {C:dark_edition}edition",
+                    "to the next {C:attention}#1#",
+                    "{C:attention}perfectly{} caught {C:fac_fish}Fish{}"
+                },
+                flavor = {
+                    "Typical French Cuisine",
+                }
+            },
+        }
+    },
+    misc = {
+        dictionary = {
+            k_fac_am_card = 'Card',
+            k_fac_am_rerolls = 'Rerolls'
+        },
+        v_dictionary = {
+            a_fac_am_rerolls = '#1# Rerolls',
+            a_fac_am_blank_left = '#1# Left',
+        }
+    }
+}
