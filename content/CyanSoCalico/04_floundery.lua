a9e09684026b4e85907170e5da40fb9c6735239c
Eremel <richardbailey@outlook.com>
2026-08-12 22:44:19 +0100

    fix tooltip crash

--- content/CyanSoCalico/04_floundery.lua
+++ content/CyanSoCalico/04_floundery.lua
@@ -149,7 +149,7 @@ FishAndChips.Fish {
 	},
 
 	loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.chips_mod, card.ability.extra.chips_mod*(math.max(0, #G.fac_fish_area.cards - #G.jokers.cards)) } }
+		return { vars = { card.ability.extra.chips_mod, card.ability.extra.chips_mod*(math.max(0, (G.fac_fish_area and #G.fac_fish_area.cards or 0) - (G.jokers and #G.jokers.cards or 0))) } }
 	end,
 	
 	calculate = function(self, card, context)
