6f3345a1ad5e44a41a53f8953a750ed99eed3133
ThunderEdge <alvindong9913@gmail.com>
2026-08-11 19:49:26 -0500

    oops forgot to check the id

--- content/thunder_and_aiko/main.lua
+++ content/thunder_and_aiko/main.lua
@@ -942,7 +942,7 @@ FishAndChips.Fish({
 		}
 	end,
 	calculate = function(self, card, context)
-		if context.individual and context.cardarea == G.play then
+		if context.individual and context.cardarea == G.play and context.other_card:get_id() == 14 then
 			return {
 				xchips = card.ability.extra.xchips,
 				-- sound_override = ...
