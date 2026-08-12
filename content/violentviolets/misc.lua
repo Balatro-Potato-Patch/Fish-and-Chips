f79ea496ce61433d2c28d0b0d7700a9ea03a47b3
WilsontheWolf <git@shorty.systems>
2026-08-12 17:34:25 -0600

    Violent Violets (FireIce + Willow) Submission

---
+++ content/violentviolets/misc.lua
@@ -0,0 +1,40 @@
+SMODS.Atlas {
+    key = 'vv_fish',
+    px = 71,
+    py = 95,
+    path = 'violentviolets/fish.png'
+}
+
+SMODS.Atlas {
+    key = 'vv_leviathan',
+    px = 71,
+    py = 198,
+    path = 'violentviolets/leviathan.png'
+}
+
+SMODS.Atlas {
+    key = 'vv_fireicecredit',
+    px = 71,
+    py = 95,
+    path = 'violentviolets/fireiceshadows1x.png'
+}
+
+SMODS.Atlas {
+    key = 'vv_willowcredit',
+    px = 71,
+    py = 95,
+    path = 'violentviolets/willowcredit.png'
+}
+
+SMODS.Sound {
+    key = 'vv_mysterygo',
+    path = 'violentviolets/snd_mysterygo.wav',
+    volume = 1,
+    pitch = 1
+}
+SMODS.Sound {
+    key = 'vv_fish',
+    path = 'violentviolets/fish.mp3',
+    volume = 1,
+    pitch = 1
+}
---
+++ localization/violentviolets/en-us.lua
@@ -0,0 +1,146 @@
+return {
+    descriptions = {
+        fac_Fish = {
+                fish_fac_vv_fireicefish = {
+                    name = "FireIce",
+                    text = {
+                        {
+                            "{X:attention,C:white}X#1#{} Blind Requirement"
+                        }
+                    },
+                    flavour = {
+                        "{C:purple}Somehow... I wound up here, don't ask."
+                    }
+                },
+                fish_fac_vv_willowfish = {
+                    name = "Kai",
+                    text = {
+                        {
+                            "Gains {X:chips,C:white}X#2#{} Chips per {C:blue}Fish{C:attention} caught{} this run",
+                            "{C:inactive}Will only increment if {C:attention}Kai {C:inactive}is present",
+                            "{C:inactive}Currently {X:chips,C:white}X#1#{C:inactive} Chips"
+                        }
+                    },
+                    flavour = {
+                        "{C:green}...can you let me go? This is really annoying."
+                    }
+                },
+                fish_fac_vv_blahaj = {
+                    name = "BLAHAJ",
+                    text = {
+                        {
+                            "{C:dark_edition}Editioned{} Jokers give {X:mult,C:white}X#1#{} Mult"
+                        }
+                    },
+                    flavour = {
+                        "True symbol of {C:blue}tr{C:fac_sand_dollars}ans {C:white}iden{C:fac_sand_dollars}ti{C:blue}ty",
+                        "Found in Sweden"
+                    }
+                },
+                fish_fac_vv_stinkyboot = {
+                    name = "Stinky Ol' Boot",
+                    text = {
+                        {
+                            "{C:chips}+1{} Chip for every Sand Dollar you have",
+                            "{C:inactive}Currently: {C:chips}+#1#{C:inactive} Chips"
+                        },{
+                            "Sells for 0 {C:fac_sand_dollars}Sand Dollars{}"
+                        }
+                    },
+                    flavour = {
+                        "Lost in the waters by some",
+                        "long forgotten fisherman.",
+                        "It was underwater at least a decade..."
+                    }
+                },
+                fish_fac_vv_winggaster = {
+                    name = "Whale Darter Gaster",
+                    text = {
+                        {
+                            "{C:chips}+#1#{} Chips",
+                            "{C:red,E:1}Self-destructs{} at end of round"
+                        }
+                    },
+                    flavour = {
+                        "That fish sure is a mystery, man."
+                    }
+                },
+                fish_fac_vv_immortalsnail = {
+                    name = "The Immortal Snail",
+                    text = {
+                        {
+                            "On Boss Blind select, destroy a random {C:blue}Fish{} and gain {X:mult,C:white}X#2#{} Mult",
+                            "{C:inactive}Currently: {X:mult,C:white}X#1#{C:inactive} Mult"
+                        }
+                    },
+                    flavour = {
+                        "It always knows where you are and slowly crawls towards you.",
+                        "{s:1.5}What's your plan?"
+                    }
+                },
+                fish_fac_vv_seashroom = {
+                    name = "Sea Shroom",
+                    text = {
+                        {
+                            "{C:spades}Spades{} and {C:clubs}Clubs{} are {C:attention}retriggered{} once",
+                            "{C:red}Debuffed{} if a {C:hearts}Heart{} or {C:diamonds}Diamond{} is scored"
+                        }
+                    },
+                    flavour = {
+                        "Sea-Shroom has never seen the sea. It's in his name,",
+                        "he's heard loads about it, but he's just never found the time."
+                    }
+                },
+                fish_fac_vv_blobfish = {
+                    name = "Blobfish",
+                    text = {
+                        {
+                            "Give a random {C:blue}Fish{} either {C:dark_edition}Foil{} or {C:dark_edition}Holographic{}",
+                        }
+                    },
+                    flavour = {
+                        "It gained the reputation of being",
+                        "the ugliest fish, but it doesn't care."
+                    }
+                },
+                fish_fac_vv_leviathan = {
+                    name = "The Leviathan",
+                    text = {
+                        {
+                            "{C:red}Destroy{} all playing cards on {C:attention}last scored hand{}",
+                            'Gain {C:fac_sand_dollars,f:fac_sand_dollars}+$1{} for each card destroyed'
+                        }
+                    },
+                    flavour = {
+                        "Be careful around its jaws, you don't want to",
+                        "lose, well... everything"
+                    }
+                },
+                fish_fac_vv_fish = {
+                    name = "The Fish",
+                    text = {
+                        {
+                            "On use, create a random {C:tarot}Tarot{}, {C:planet}Planet{} or {C:spectral}Spectral{} card",
+                            "{C:inactive,s:0.8}(Must have room)"
+                        }
+                    },
+                    flavour = {
+                        "You know what that means."
+                    }
+                },
+                fish_fac_vv_jetfish = {
+                    name = "Jetfish",
+                    text = {
+                        {
+                            "If all played cards score on {C:attention}last played hand",
+                            "Each card gives {C:mult}+#1#{} Mult"
+                        }
+                    },
+                    flavour = {
+                        "This creature is dehydrated.",
+                        "You can carry it back to water."
+                    }
+                },
+        }
+    }
+}
