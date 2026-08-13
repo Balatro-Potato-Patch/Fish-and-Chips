ecd812dd5c8b0d0e0fb9808bd31a0091e0f06d6c
WilsontheWolf <git@shorty.systems>
2026-08-12 19:21:04 -0600

    Nxkoo - Feli Submission

---
+++ localization/nxkooli/en-us.lua
@@ -0,0 +1,147 @@
+return{
+    descriptions = {
+        fac_Fish = {
+            fish_fac_nxkooli_ins_good_fish = {
+                name = "Good Fish",
+                text = {
+                    {
+                        "{C:chips}+#1#{} Chips, {C:mult}+#2#{} Mult",
+                    },
+                    {
+                        "{X:blind,C:white}X#3#{} Blind Req.",
+                        "when entering a",
+                        "{C:attention}Boss Blind"
+                    }
+                },
+                flavor = {
+                    "Fish. Fresh fish. Good.",
+                    "Still. Must battle."
+                }
+            },
+            fish_fac_nxkooli_ins_bad_fish = {
+                name = "Bad Fish",
+                text = {
+                    {
+                        "Lose half your",
+                        "{C:fac_sand_dollars}Sand Dollars{} when caught",
+                        "{C:mult}+#1#{} Mult",
+                    },
+
+                },
+                flavor = {
+                    "Hm. Stale fish. Hard choose.",
+                }
+            },
+            fish_fac_nxkooli_ins_more_fish = {
+                name = "More Fish",
+                text = {
+                    {
+                        "{C:mult}+#1#{} Mult",
+                    },
+                    {
+                        "When this fish gets {C:red,E:2}removed{},",
+                        "create a random {C:attention}Inscryption{} fish"
+                    }
+                },
+                flavor = {
+                    "Take new fish. Easy.",
+                }
+            },
+            fish_fac_nxkooli_garfield_phone = {
+                name = {
+                    "Garfield Corded Telephone",
+                    "Tyco Model 1207",
+                },
+                text = {
+                    {
+                        "something"
+                    }
+                },
+                flavor = {
+                    "A pristine corded telephone of the World\'s",
+                    "most famous cartoon feline.",
+                    "",
+                    "It's brothers aren\'t as nicely preserved,",
+                    "in the depths of Brittany's waters."
+                }
+            },
+            fish_fac_nxkooli_garfield_fish = {
+                name = {
+                    "Spotted Gar-f",
+                },
+                text = {
+                    {
+                        "After catching fish",
+                        "earn {C:fac_sand_dollars,f:fac_sand_dollars}${C:fac_sand_dollars}#1#{}",
+                        "{s:0.5} ",
+                        "Earn {C:fac_sand_dollars,f:fac_sand_dollars}${C:fac_sand_dollars}#2#{} if it's a",
+                        "{C:dark_edition}perfect catch"
+                    }
+                },
+                flavor = {
+                    "I can\'t be underwater...",
+                    "I\'m a cat!",
+                    "Wait no, I\'m a fish!",
+                    "Wait no, I\'m a catfish!",
+                }
+            },
+            fish_fac_nxkooli_pp_tile = {
+                name = {
+                    "Fisher\'s Tile",
+                },
+                text = {
+                    {
+                        "{V:1}+#3#{} #4# for #7# {C:attention}#6#{}",
+                        "in the {C:attention}name{} and {C:attention}flavor text{}",
+                        "of the {C:planet}Fish{} to the right",
+                        "{C:inactive,s:0.8}(Tile changes every hand)",
+                        "{C:inactive}(Currently {V:1}+#8#{C:inactive} #4#)"
+
+                        
+                    }
+                },
+                flavor = {
+                    "Previously a weapon of the oppressed.",
+                    "Now merely a reminder of the Party\'s","past influence.",
+                }
+            },
+            fish_fac_nxkooli_pp_tile_crit = {
+                name = {
+                    "Fisher\'s Tile",
+                },
+                text = {
+                    {
+                        "{V:1}+#3#{} #4# and",
+                        "{X:1,C:white}+X#5#{} #4# for #7# {C:attention}#6#{}",
+                        "in the {C:attention}name{} and {C:attention}flavor text{}",
+                        "of the {C:planet}Fish{} to the right",
+                        "{C:inactive,s:0.8}(Tile changes every hand)",
+                        "{C:inactive}(Currently {V:1}+#8#{C:inactive}, {X:1,C:white}X#9#{C:inactive} #4#)"
+                        
+                    }
+                },
+                flavor = {
+                    "Previously a weapon of the oppressed.",
+                    "Now merely a reminder of the Party\'s",
+                    "past influence.",
+                }
+            },
+        },
+        PotatoPatch = {
+            PotatoPatchDev_Nxkoo = {
+                name = 'ooʞxͶ',
+                text = {
+                    "{C:inactive}* Please forget about me."
+                }
+            },
+            PotatoPatchDev_LasagnaFelidae = {
+                name = 'LasagnaFelidae',
+                text = {
+                    "meow"
+                }
+            }
+        }
+    }
+
+
+}
