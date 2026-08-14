4dd58c61c268304db5c088a7a0e45c9b602d8989
GhostSalt <ghostsalt12@gmail.com>
2026-08-14 18:43:43 +0100

    -Astra/MissingNo: Localisation tweaks

--- localization/astra-missingno/en-us.lua
+++ localization/astra-missingno/en-us.lua
@@ -1,38 +1,38 @@
 return {
     descriptions = {
-		PotatoPatch = {
-			PotatoPatchDev_theAstra = {
-				name = "theAstra",
+        PotatoPatch = {
+            PotatoPatchDev_theAstra = {
+                name = "theAstra",
                 text = {
-                    {"Howdy! I'm {C:purple,E:2}Astra{}, head of the {C:attention}Potato Patch!",
-                    "Thanks for playing the mod! This was easily our {C:red}most ambitious",
-                    "event mod yet, and I had a blast making it happen! Shout out to",
-                    "my fellow {C:attention}Organizers{} and the {C:planet}Guest Dev{} team for bringing this crazy",
-                    "vision to life!",
-                    "{C:dark_edition,s:1.4}To The Stars!!! {C:dark_edition,s:1.4,f:8}🪐"},
-                    {"Also check out this Brooke Trout I caught irl",
-                    "during the development of this mod",
-                    "{element:1}"}
+                    { "Howdy! I'm {C:purple,E:2}Astra{}, head of the {C:attention}Potato Patch!",
+                        "Thanks for playing the mod! This was easily our {C:red}most ambitious",
+                        "event mod yet, and I had a blast making it happen! Shout out to",
+                        "my fellow {C:attention}Organizers{} and the {C:planet}Guest Dev{} team for bringing this crazy",
+                        "vision to life!",
+                        "{C:dark_edition,s:1.4}To The Stars!!! {C:dark_edition,s:1.4,f:8}🪐" },
+                    { "Also check out this Brooke Trout I caught irl",
+                        "during the development of this mod",
+                        "{element:1}" }
                 }
-			},
-			PotatoPatchDev_MissingNo = {
-				name = "MissingNo",
+            },
+            PotatoPatchDev_MissingNo = {
+                name = "MissingNo",
                 text = {
                     "{C:inactive,s:0.8}#1#",
                     "Artist for {C:red}0 ERROR{} and {C:purple}Finity",
                     "unhinged internet idiot",
                     "Listen to {C:blue,u:blue}my music{} to die instantly"
                 }
-			},
+            },
         },
         fac_Fish = {
             fish_fac_am_jerry = {
                 name = "Jerry",
                 text = {
                     "Gives {C:chips}Chips{} equal",
-                    "to {C:attention}x#1#{} the current",
+                    "to {C:attention}X#1#{} the current",
                     "{C:attention}Music Volume{} setting",
-                    "{C:inactive,s:0.8}Currently: {s:0.8,C:chips}+#2# {C:inactive,s:0.8}Chips"
+                    "{C:inactive}(Currently {C:chips}+#2#{C:inactive} Chips)"
                 },
                 flavor = {
                     "This music-loving jellyfish",
@@ -43,12 +43,12 @@ return {
             fish_fac_am_king = {
                 name = "King of the Pond",
                 text = {
-                    "{X:red,C:white}X#1#{} Mult, gains {X:red,C:white}X#2#{} Mult for",
-                    "each played and scoring {C:attention}King{}",
+                    "This {C:fac_fish}Fish{} gains {X:mult,C:white}X#2#{} Mult",
+                    "per played and scored {C:attention}King{}",
+                    "{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive} Mult)"
                 },
                 flavor = {
-                    "There can only be one",
-                    "true king",
+                    "There can only be one true king"
                 }
             },
             fish_fac_am_missingno = {
@@ -66,9 +66,10 @@ return {
             fish_fac_am_shrimp = {
                 name = "Shrimp Scamperer",
                 text = {
-                    "{C:mult}+#1#{} Mult, gains {C:mult}+#2#{} Mult if a",
-                    " hand type is played {C:attention}#3#{C:inactive}[#4#]{} times",
-                    "{C:inactive,s:0.8}Current hand: #5#"
+                    "This {C:fac_fish}Fish{} gains {C:mult}+#2#{} Mult when",
+                    "a hand type is played {C:attention}#3#{} {C:inactive}[#4#]{} times",
+                    "{C:inactive}(Current hand: {C:attention}#5#{C:inactive})",
+                    "{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult){}"
                 },
                 flavor = {
                     "One! Two! Three!",
@@ -80,7 +81,7 @@ return {
                 text = {
                     "{C:attention}Retriggers{} played cards",
                     "with non-number ranks",
-                    "{C:attention}#1#{} time"
+                    "{C:attention}#1#{} additional time"
                 },
                 flavor = {
                     " _",
@@ -90,10 +91,9 @@ return {
             fish_fac_am_teabag = {
                 name = "Teabag",
                 text = {
-                    "Gives {C:attention}1",
-                    "selected card ",
-                    "permanent {C:money}$#1#{}",
-                    "when scored",
+                    "Gives {C:attention}1{} selected",
+                    "card a permanent",
+                    "{C:money}$#1#{} when scored",
                     "{ppu_bubble:usable}"
                 },
                 flavor = {
@@ -112,8 +112,7 @@ return {
                 },
                 flavor = {
                     "Its tentacles dangle with",
-                    "anticipation of a meal",
-                    "floating by"
+                    "anticipation of a meal floating by"
                 }
             },
             fish_fac_am_chameleon = {
@@ -121,7 +120,7 @@ return {
                 text = {
                     "Copies the",
                     "{C:attention}centermost{} Joker",
-                    "{C:inactive,s:0.8}Must have odd number of Jokers"
+                    "{C:inactive,s:0.8}(Must have odd number of Jokers)"
                 },
                 flavor = {
                     "It attempts to blend into",
@@ -132,26 +131,29 @@ return {
             fish_fac_am_mola = {
                 name = "Mola Mola",
                 text = {
-                    {"Disables effect of",
-                    "every {C:attention}Boss Blind",},
-                    {"{C:green}#1# in #2#{} chance to {C:red,E:1}die{}",
-                    "at the end of the {C:attention}shop{}"}
+                    {
+                        "Disables effect of",
+                        "every {C:attention}Boss Blind"
+                    },
+                    {
+                        "{C:green}#1# in #2#{} chance to {C:red,E:1}die{}",
+                        "at the end of the {C:attention}shop{}"
+                    }
                 },
                 flavor = {
-                    "While it's great at",
-                    "deterring parasites,",
-                    "it's quite terrible at",
-                    "literally everything else"
+                    "While it's great at deterring",
+                    "parasites, it's quite terrible",
+                    "at literally everything else"
                 }
             },
             fish_fac_am_dopefish = {
                 name = "Dopefish",
                 text = {
-                    "When blind is selected,",
-                    "this {C:fac_fish}Fish {C:red,E:1}eats{} all",
-                    "{C:attention}adjacent {C:fac_fish}Fish{} and gains",
-                    "{X:red,C:white}X#2#{} for each {C:fac_fish}Fish{} eaten",
-                    "{C:inactive,s:0.8}Currently: {X:red,C:white,s:0.8}X#1#{C:inactive,s:0.8} Mult"
+                    "When {C:attention}Blind{} is selected, this",
+                    "{C:fac_fish}Fish{} {C:red,E:1}eats{} all {C:attention}adjacent{}",
+                    "{C:fac_fish}Fish{} and gains {X:mult,C:white}X#2#{} Mult",
+                    "for each {C:fac_fish}Fish{} eaten",
+                    "{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive} Mult)"
                 },
                 flavor = {
                     "The second dumbest",
@@ -163,14 +165,18 @@ return {
             fish_fac_am_piscis = {
                 name = "Piscis Austrinus",
                 text = {
-                    {"Use this {C:fac_fish}Fish{} to upgrade",
-                    "the level of the {C:attention}last played{}",
-                    "poker hand #1# times",
-                    "{ppu_bubble:usable}"},
-                    {"{S:0.8}({S:0.8,V:1}lvl.#2#{S:0.8}){} Level up",
-                    "{C:attention}#3#",
-                    "{C:mult}+#4#{} Mult and",
-                    "{C:chips}+#5#{} chips",}
+                    {
+                        "Use this {C:fac_fish}Fish{} to upgrade",
+                        "the level of the {C:attention}last played{}",
+                        "poker hand {C:attention}#1#{} times",
+                        "{ppu_bubble:usable}"
+                    },
+                    {
+                        "{S:0.8}({S:0.8,V:1}lvl.#2#{S:0.8}){} Level up",
+                        "{C:attention}#3#",
+                        "{C:mult}+#4#{} Mult and",
+                        "{C:chips}+#5#{} chips",
+                    }
                 },
                 flavor = {
                     "It is said that gazing at the",
@@ -182,10 +188,10 @@ return {
             fish_fac_am_blubby = {
                 name = "Blubby",
                 text = {
-                    "{C:attention}1{} free {C:green}Reroll",
-                    "for every {C:attention}#1#%{} of score",
+                    "{C:attention}1{} free {C:green}Reroll{} for",
+                    "every {C:attention}#1#%{} of score",
                     "overshot each round",
-                    "{C:inactive,s:0.8}Currently: {C:green,s:0.8}#2# Rerolls",
+                    "{C:inactive}(Currently {C:green}#2#{C:inactive} Rerolls)",
                 },
                 flavor = {
                     "It seems like this Fish came",
@@ -195,11 +201,11 @@ return {
                 }
             },
             fish_fac_am_chocolat = {
-                name = {"Le Fishe", "au chocolat"},
+                name = { "Le Fishe", "au chocolat" },
                 text = {
                     "Applies a {C:green}random{} {C:dark_edition}edition",
-                    "to the next {C:attention}#1#",
-                    "{C:attention}perfectly{} caught {C:fac_fish}Fish{}"
+                    "to the next {C:attention}#1# {C:fac_fish}Fish{}",
+                    "caught {C:attention}perfectly{}"
                 },
                 flavor = {
                     "Typical French Cuisine",
