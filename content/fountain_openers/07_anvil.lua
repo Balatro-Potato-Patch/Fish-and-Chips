1a65d76a5b466816f1a482294d5aff5e2286136e
Eremel <richardbailey@outlook.com>
2026-08-14 01:05:49 +0100

    Remove these for now

--- content/fountain_openers/07_anvil.lua
+++ content/fountain_openers/07_anvil.lua
@@ -258,51 +258,51 @@ G.FUNCS.fac_fo_fucking_kill_fish = function(e)
     FountainOpeners.anvil_animation:play(card)
 end
 
-local uasb = G.UIDEF.use_and_sell_buttons
-function G.UIDEF.use_and_sell_buttons(card)
-    local ret = uasb(card)
+-- local uasb = G.UIDEF.use_and_sell_buttons
+-- function G.UIDEF.use_and_sell_buttons(card)
+--     local ret = uasb(card)
 
-    if card.ability.set == 'fac_Fish' and card.fac_fo_anvil then
-        local sell = {n=G.UIT.C, config={align = "cr"}, nodes={
-            {n=G.UIT.C, config={ref_table = card, align = "cr",padding = 0.1, r=0.08, minw = 1.25, hover = true, shadow = true, colour = G.C.UI.BACKGROUND_INACTIVE, one_press = true, button = 'sell_card', func = 'can_sell_card', handy_insta_action = 'sell'}, nodes={
-                {n=G.UIT.B, config = {w=0.1,h=0.6}},
-                {n=G.UIT.C, config={align = "tm"}, nodes={
-                    {n=G.UIT.R, config={align = "cm", maxw = 1.25}, nodes={
-                        {n=G.UIT.T, config={text = localize('b_sell'),colour = G.C.UI.TEXT_LIGHT, scale = 0.4, shadow = true}}
-                    }},
-                    {n=G.UIT.R, config={align = "cm"}, nodes={
-                        {n=G.UIT.T, config={text = localize('$'), colour = G.C.WHITE, scale = 0.55, shadow = true, font = SMODS.Fonts["fac_sand_dollars"]}},
-                        {n=G.UIT.T, config={ref_table = card, ref_value = 'sell_cost_label',colour = G.C.WHITE, scale = 0.55, shadow = true}}
-                    }}
-                }}
-            }},
-        }}
-        local take = {n=G.UIT.C, config={align = "cr"}, nodes={
-            {n=G.UIT.C, config={ref_table = card, align = "cm",padding = 0.1, r=0.08, minw = 1.25, minh = 0.8, hover = true, shadow = true, colour = G.C.UI.BACKGROUND_INACTIVE, fac_ignore = true, button = 'fac_fo_take_fish', func = "fac_fo_can_take_fish", handy_insta_action = 'use'}, nodes={
-                {n=G.UIT.B, config = {w=0.1,h=0.6}},
-                {n=G.UIT.C, config={align = "cm"}, nodes={
-                    {n=G.UIT.R, config={align = "cm", maxw = 1.25}, nodes={
-                        {n=G.UIT.T, config={text = card.config.center.button_key and (type(card.config.center.button_key) == "function" and card.config.center:button_key() or localize(card.config.center.button_key)) or localize("fac_fo_take"), colour = G.C.UI.TEXT_LIGHT, scale = 0.55, shadow = true}}
-                    }},
-                }},
-            }},
-        }}
-        ret = {n=G.UIT.ROOT, config = {padding = 0, colour = G.C.CLEAR}, nodes={
-            {n=G.UIT.C, config={padding = 0.15, align = 'cl'}, nodes={
-                {n=G.UIT.R, config={align = 'cl'}, nodes={
-                    sell
-                }},
-                {n=G.UIT.R, config={align = 'cl'}, nodes={
-                    take
-                }},
-                {n=G.UIT.R, config={ref_table = card, r = 0.08, padding = 0.1, align = 'cl', minw = 0.5*card.T.w - 0.15, maxw = 0.9*card.T.w - 0.15, minh = 0.3*card.T.h, hover = true, shadow = true, colour = G.C.UI.BACKGROUND_INACTIVE,
-                one_press = true, button = 'fac_fo_fucking_kill_fish', func = 'fac_fo_can_fucking_kill_fish'}, nodes={
-                    {n=G.UIT.O, config={object=
-                        SMODS.create_sprite(0, 0, 377 / 255, 105 / 255, "fac_fo_fucking_kill", {x = 0, y = 0})
-                    }}
-                }},
-            }},
-        }}
-    end
-    return ret
-end
+--     if card.ability.set == 'fac_Fish' and card.fac_fo_anvil then
+--         local sell = {n=G.UIT.C, config={align = "cr"}, nodes={
+--             {n=G.UIT.C, config={ref_table = card, align = "cr",padding = 0.1, r=0.08, minw = 1.25, hover = true, shadow = true, colour = G.C.UI.BACKGROUND_INACTIVE, one_press = true, button = 'sell_card', func = 'can_sell_card', handy_insta_action = 'sell'}, nodes={
+--                 {n=G.UIT.B, config = {w=0.1,h=0.6}},
+--                 {n=G.UIT.C, config={align = "tm"}, nodes={
+--                     {n=G.UIT.R, config={align = "cm", maxw = 1.25}, nodes={
+--                         {n=G.UIT.T, config={text = localize('b_sell'),colour = G.C.UI.TEXT_LIGHT, scale = 0.4, shadow = true}}
+--                     }},
+--                     {n=G.UIT.R, config={align = "cm"}, nodes={
+--                         {n=G.UIT.T, config={text = localize('$'), colour = G.C.WHITE, scale = 0.55, shadow = true, font = SMODS.Fonts["fac_sand_dollars"]}},
+--                         {n=G.UIT.T, config={ref_table = card, ref_value = 'sell_cost_label',colour = G.C.WHITE, scale = 0.55, shadow = true}}
+--                     }}
+--                 }}
+--             }},
+--         }}
+--         local take = {n=G.UIT.C, config={align = "cr"}, nodes={
+--             {n=G.UIT.C, config={ref_table = card, align = "cm",padding = 0.1, r=0.08, minw = 1.25, minh = 0.8, hover = true, shadow = true, colour = G.C.UI.BACKGROUND_INACTIVE, fac_ignore = true, button = 'fac_fo_take_fish', func = "fac_fo_can_take_fish", handy_insta_action = 'use'}, nodes={
+--                 {n=G.UIT.B, config = {w=0.1,h=0.6}},
+--                 {n=G.UIT.C, config={align = "cm"}, nodes={
+--                     {n=G.UIT.R, config={align = "cm", maxw = 1.25}, nodes={
+--                         {n=G.UIT.T, config={text = card.config.center.button_key and (type(card.config.center.button_key) == "function" and card.config.center:button_key() or localize(card.config.center.button_key)) or localize("fac_fo_take"), colour = G.C.UI.TEXT_LIGHT, scale = 0.55, shadow = true}}
+--                     }},
+--                 }},
+--             }},
+--         }}
+--         ret = {n=G.UIT.ROOT, config = {padding = 0, colour = G.C.CLEAR}, nodes={
+--             {n=G.UIT.C, config={padding = 0.15, align = 'cl'}, nodes={
+--                 {n=G.UIT.R, config={align = 'cl'}, nodes={
+--                     sell
+--                 }},
+--                 {n=G.UIT.R, config={align = 'cl'}, nodes={
+--                     take
+--                 }},
+--                 {n=G.UIT.R, config={ref_table = card, r = 0.08, padding = 0.1, align = 'cl', minw = 0.5*card.T.w - 0.15, maxw = 0.9*card.T.w - 0.15, minh = 0.3*card.T.h, hover = true, shadow = true, colour = G.C.UI.BACKGROUND_INACTIVE,
+--                 one_press = true, button = 'fac_fo_fucking_kill_fish', func = 'fac_fo_can_fucking_kill_fish'}, nodes={
+--                     {n=G.UIT.O, config={object=
+--                         SMODS.create_sprite(0, 0, 377 / 255, 105 / 255, "fac_fo_fucking_kill", {x = 0, y = 0})
+--                     }}
+--                 }},
+--             }},
+--         }}
+--     end
+--     return ret
+-- end
