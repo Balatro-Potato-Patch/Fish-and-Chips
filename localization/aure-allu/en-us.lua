bb799bfae513d5e8d7cad8aaf475396d96743263
GhostSalt <ghostsalt12@gmail.com>
2026-08-15 13:55:16 +0100

    -Aure / AllUniversal: Almost done polishing these fish

--- localization/aure-allu/en-us.lua
+++ localization/aure-allu/en-us.lua
@@ -32,19 +32,20 @@ return {
         },
         fac_Fish = {
             fish_fac_aureallu_the_original___starfish = {
-                name = {"2 Original    ", "    2 Starfish"},
+                name = { "2 Original    ", "    2 Starfish" },
                 text = {
                     "Use this {V:1}Star{C:fac_fish}fish{}",
                     "for {C:attention}#1# {C:planet}levels{} 2 most",
                     "played {C:attention}poker hand",
-                    "{C:inactive,s:0.8}(Currently {C:attention,s:0.8}#2#{C:inactive,s:0.8})"
+                    "{C:inactive,s:0.8}(Currently {C:attention,s:0.8}#2#{C:inactive,s:0.8})",
+                    "{ppu_bubble:usable}"
                 },
                 flavor = {
                     "These fish are pissing me off",
                 }
             },
             fish_fac_aureallu_cheap_cheep = {
-                name = {"Cheap Cheep"},
+                name = "Cheap Cheep",
                 text = {
                     "{C:green}#1# in #2#{} chance",
                     "to {C:attention}refund {C:fac_sand_dollars,f:fac_sand_dollars}$#3#",
@@ -56,10 +57,10 @@ return {
                 }
             },
             fish_fac_aureallu_blooper = {
-                name = {"Blooper"},
+                name = "Blooper",
                 text = {
                     {
-                        "Blubs {C:white,X:chips}+X#1#{} Chips",
+                        "Blubs {C:white,X:chips}X#1#{} Chips",
                         "per {C:attention}face down{} card",
                         "held in hand",
                         "{C:inactive}(Currently {C:white,X:chips}X#2#{C:inactive} Chips)"
@@ -75,7 +76,7 @@ return {
                 }
             },
             fish_fac_aureallu_goldfish = {
-                name = {"Goldfish"},
+                name = "Goldfish",
                 text = {
                     "Gains {C:fac_sand_dollars,f:fac_sand_dollars}$#1#{} of",
                     "{C:money}sell value{} at",
@@ -86,7 +87,7 @@ return {
                 }
             },
             fish_fac_aureallu_moldfish = {
-                name = {"Moldfish"},
+                name = "Moldfish",
                 text = {
                     "Blubs {C:mult}#1#{} Mult times the",
                     "{C:attention}average{} number of {C:attention}rounds{}",
@@ -98,7 +99,7 @@ return {
                 }
             },
             fish_fac_aureallu_shrimp = {
-                name = {"Shrimp"},
+                name = "Shrimp",
                 text = {
                     "Gains {C:mult}#1#{} Mult if",
                     "this {C:fac_fish}Shrimp{} resides in",
@@ -112,7 +113,7 @@ return {
                 }
             },
             fish_fac_aureallu_mult_mola = {
-                name = {"Mult Mola"},
+                name = "Mult Mola",
                 text = {
                     "Blubs {C:mult}#1#{} Mult for",
                     "each empty {C:attention}Bucket slot",
@@ -123,96 +124,95 @@ return {
                 }
             },
             fish_fac_aureallu_eel_of_fortune = {
-                name = {"Eel of Fortune"},
+                name = "Eel of Fortune",
                 text = {
-                    "Use for {C:green}#1# in #2#{} chance to",
-                    "add {C:dark_edition}Foil{}, {C:dark_edition}Holographic{}, or",
-                    "{C:dark_edition}Polychrome{} to a random {C:fac_fish}Fish"
+                    "Use for a {C:green}#1# in #2#{} chance",
+                    "to add {C:dark_edition}Foil{}, {C:dark_edition}Holographic{}, or",
+                    "{C:dark_edition}Polychrome{} to a random {C:fac_fish}Fish",
+                    "{ppu_bubble:usable}"
                 },
                 flavor = {
                     "Big money no swamp-eels"
                 }
             },
             fish_fac_aureallu_gouramichel = {
-                name = {"Gouramichel"},
+                name = "Gouramichel",
                 text = {
-                    "{C:chips}#1#{} Chips", 
+                    "{C:chips}#1#{} Chips",
                     "{C:green}#2# in #3#{} chance",
-                    "this {C:fac_fish}Fish{} dips",
+                    "this {C:fac_fish}Fish{} {C:red,E:2}dips{}",
                     "at {C:attention}end of round",
-                },
-                flavor = {
-                    ""
                 }
             },
             fish_fac_aureallu_cavenfish = {
-                name = {"Cavenfish"},
+                name = "Cavenfish",
                 text = {
-                    "{X:chips,C:white}X#1#{} Chips", 
+                    "{X:chips,C:white}X#1#{} Chips",
                     "{C:green}#2# in #3#{} chance",
-                    "this {C:fac_fish}Fish{} dips",
+                    "this {C:fac_fish}Fish{} {C:red,E:2}dips{}",
                     "at {C:attention}end of round",
-                },
-                flavor = {
-                    ""
                 }
             },
             fish_fac_aureallu_hammerjaw = {
-                name = {"Hammerjaw"},
+                name = "Hammerjaw",
                 text = {
                     "Use this {C:fac_fish}Fish{} to",
                     "{C:attention}destroy{} up to {C:attention}#1#{}",
                     "selected cards,",
-                    "and then {C:attention}redraw"
+                    "and then {C:attention}redraw",
+                    "{ppu_bubble:usable}"
                 },
                 flavor = {
                     "Stop! Hammer time."
                 }
             },
             fish_fac_aureallu_blue_garden_gnome = {
-                name = {"Blue Garden Gnome"},
+                name = "Blue Garden Gnome",
                 text = {
                     "{C:white,X:money}X#1#{C:money} Treasure{} rewards",
-                    "and catch speed"                
+                    "and catch speed"
                 },
                 flavor = {
                     "Hooh!"
                 }
             },
             fish_fac_aureallu_bat_ray = {
-                name = {"Bat Ray"},
+                name = "Bat Ray",
                 text = {
                     "Use this {C:fac_fish}Ray{} to",
                     "{C:attention}flip{} up to {C:attention}#1#{} selected",
-                    "{C:attention}Playing{} cards in hand",
+                    "{C:attention}playing cards{} in hand",
                     "{C:inactive,s:0.9}(Currently {C:attention,s:0.9}#2#{C:inactive,s:0.9}/#3#{C:inactive,s:0.9} uses)",
-                    "{C:inactive,s:0.8}(Resets at {C:attention,s:0.8}end of round{C:inactive,s:0.8})"
-                    
+                    "{C:inactive,s:0.8}(Resets at {C:attention,s:0.8}end of round{C:inactive,s:0.8})",
+                    "{ppu_bubble:usable}"
+
                 },
                 flavor = {
-                    "To the batmobile!"
+                    "To the Batmobile!"
                 }
             },
             fish_fac_aureallu_cowfish = {
-                name = {"Cowfish"},
+                name = "Cowfish",
                 text = {
                     "Every {C:attention}#1#{C:inactive}/#2#{} rounds,",
                     "{C:attention}milk{} this {C:fac_fish}Fish{} to create",
-                    "a random {C:spectral}Spectral{} card"
+                    "a random {C:spectral}Spectral{} card",
+                    "{C:inactive}(Must have room){}"
                 },
                 flavor = {
                     "Moo."
                 }
             },
             fish_fac_aureallu_soldierfish = {
-                name = {"Soldierfish"},
+                name = "Soldierfish",
                 text = {
                     "Use this {C:fac_fish}Fish{} to {C:attention}redraw",
                     "and then draw {C:attention}#1#{} cards",
                     "{C:attention}ignoring{} hand size",
-                    "{C:inactive,s:0.9}Costs {C:money,s:1.0}$#2# {C:inactive,s:0.8}Increases by {C:money,s:0.8}$#3#{C:inactive}",
-                    "{C:inactive,s:0.7}Resets to {C:attention,s:0.7}last{C:inactive,s:0.7} payed price",
-                    "{C:inactive,s:0.7}at {C:attention,s:0.7}end of round"
+                    "{C:inactive,s:0.9}Costs {C:money,s:1.0}$#2#, {C:inactive,s:0.9}increases by {C:money,s:0.8}$#3#{C:inactive}",
+                    "{C:inactive,s:0.8}Resets to last paid",
+                    "{C:inactive,s:0.8}price at {C:attention,s:0.8}end of round",
+                    "{ppu_bubble:usable}"
                 },
                 flavor = {
                     "Served for oil,",
@@ -220,18 +220,19 @@ return {
                 }
             },
             fish_fac_aureallu_unicorn_fish = {
-                name = {"Unicorn Fish"},
+                name = "Unicorn Fish",
                 text = {
                     "Use this {C:fac_fish}Fish{} for",
                     "a {C:green}#1# in #2#{} chance to",
-                    "create a {C:rare}Rare {C:attention}Tag"
+                    "create a {C:rare}Rare {C:attention}Tag",
+                    "{ppu_bubble:usable}"
                 },
                 flavor = {
                     "Neigh! (Sparkly)"
                 }
             },
             fish_fac_aureallu_clownfish = {
-                name = {"Clownfish"},
+                name = "Clownfish",
                 text = {
                     "{C:attention,E:1}Copies ability{} of {C:attention}Joker",
                     "at this {C:fac_fish}Fish{}'s position"
@@ -241,7 +242,7 @@ return {
                 }
             },
             fish_fac_aureallu_pirate_perch = {
-                name = {"Pirate Perch"},
+                name = "Pirate Perch",
                 text = {
                     "Blubs {C:chips}#1#{} Chips per",
                     "{C:fac_sand_dollars,f:fac_sand_dollars}$1{} of {C:money}sell value",
@@ -253,12 +254,12 @@ return {
                 }
             },
             fish_fac_aureallu_cookiecutter_shark = {
-                name = {"Cookiecutter Shark"},
+                name = "Cookiecutter Shark",
                 text = {
                     "{C:green}#1# in #2#{} chance for held",
-                    "{C:enhanced}Lucky {C:attention}cards{} to create a",
-                    "{C:attention}D6 Tag{} and be {C:mult}destroyed",
-                    "at {C:attention}end of round{}",                    
+                    "{C:attention}Lucky{} cards to create a",
+                    "{C:attention}D6 Tag{} and be {C:attention}destroyed",
+                    "at {C:attention}end of round{}",
                 },
                 flavor = {
                     "Warning: Do not store within",
@@ -266,20 +267,21 @@ return {
                 }
             },
             fish_fac_aureallu_chimaera = {
-                name = {"Chimaera"},
+                name = "Chimaera",
                 text = {
                     "After any {C:attention}played hand",
                     "contained a {C:attention}Flush{}, a {C:attention}Pair{}",
                     "and a {C:enhanced}Wild {C:attention}card{}, {C:attention}using",
                     "this {C:fac_fish}Fish {C:dark_edition,E:1}morphs{} it into",
-                    "the {C:attention}last{} used {C:fac_fish}Fish"
+                    "the {C:attention}last{} used {C:fac_fish}Fish",
+                    "{ppu_bubble:usable}"
                 },
                 flavor = {
                     "This Chimaera ain't arced."
                 }
             },
             fish_fac_aureallu_guppies = {
-                name = {"Guppies"},
+                name = "Guppies",
                 text = {
                     "All {C:attention}held {C:fac_fish}Fish{} have",
                     "a {C:green}#1# in #2#{} chance",
@@ -291,7 +293,7 @@ return {
                 }
             },
             fish_fac_aureallu_neunauge = {
-                name = {"Neunauge"},
+                name = "Neunauge",
                 text = {
                     "Retrigger {C:attention}all 9s{}",
                     "if there's at most",
@@ -306,21 +308,22 @@ return {
                 }
             },
             fish_fac_aureallu_old_world_knifefish = {
-                name = {"Old World", "Knifefish"},
+                name = { "Old World", "Knifefish" },
                 text = {
                     "Use this {C:fac_fish}Fish{} to",
                     "{C:red}destroy{} the {C:attention}Joker",
                     "at {C:fac_fish}its{} position, and",
                     "gain {C:white,X:money}X#1#{C:attention} its {C:money}sell value",
                     "{C:inactive,s:0.9}(Currently {C:attention,s:0.9}#2#{C:inactive,s:0.9}/#3#{C:inactive,s:0.9} uses)",
-                    "{C:inactive,s:0.8}(Resets at {C:attention,s:0.8}end of round{C:inactive,s:0.8})"
+                    "{C:inactive,s:0.8}(Resets at {C:attention,s:0.8}end of round{C:inactive,s:0.8})",
+                    "{ppu_bubble:usable}"
                 },
                 flavor = {
                     "28 stab wounds"
                 }
             },
             fish_fac_aureallu_thrasher_shark = {
-                name = {"Thrasher Shark"},
+                name = "Thrasher Shark",
                 text = {
                     "When scored, {C:attention}first",
                     "scoring card's {C:attention}Rank",
@@ -334,7 +337,7 @@ return {
                 }
             },
             fish_fac_aureallu_blahaj = {
-                name = {"Balåhaj"},
+                name = "Balåhaj",
                 text = {
                     "Use this {C:fac_fish}Fish{} to",
                     "{C:attention}change{} {C:fac_suits}suits{} of up",
@@ -342,20 +345,22 @@ return {
                     "{C:hearts,s:0.8}Hearts {C:inactive,s:0.8}<-> {C:clubs,s:0.8}Clubs",
                     "{C:diamonds,s:0.8}Diamonds {C:inactive,s:0.8}<-> {C:spades,s:0.8}Spades",
                     "{C:inactive,s:0.8}(Currently {C:attention,s:0.8}#2#{C:inactive,s:0.8}/#3#{C:inactive,s:0.8} uses)",
-                    "{C:inactive,s:0.8}(Resets at {C:attention,s:0.8}end of round{C:inactive,s:0.8})"
+                    "{C:inactive,s:0.8}(Resets at {C:attention,s:0.8}end of round{C:inactive,s:0.8})",
+                    "{ppu_bubble:usable}"
                 },
                 flavor = {
                     ":3"
                 }
             },
             fish_fac_aureallu_firefly_squid = {
-                name = {"Firefly Squid"},
+                name = "Firefly Squid",
                 text = {
                     "If {C:attention}score{} catches",
                     "on {C:fac_allu_fire_g,E:1}fire{}, creates",
                     "a {C:tarot}Tarot{} card, or",
                     "a {C:spectral}Spectral{} card",
-                    "after {C:attention}Boss Blind"
+                    "after {C:attention}Boss Blind",
+                    "{C:inactive}(Must have room)"
                 },
                 flavor = {
                     "You would not",
@@ -363,11 +368,11 @@ return {
                 }
             },
             fish_fac_aureallu_vampire_squid = {
-                name = {"Vampire Squid"},
+                name = "Vampire Squid",
                 text = {
                     "This {C:fac_fish}Squid{} gains",
                     "{X:mult,C:white}X#1#{} Mult when a",
-                    "{C:fac_fish}Fish{} is {C:mult}destroyed",
+                    "{C:fac_fish}Fish{} is {C:attention}destroyed",
                     "{C:inactive}(Currently {C:white,X:mult}X#2#{C:inactive} Mult)"
                 },
                 flavor = {
@@ -377,14 +382,15 @@ return {
                 }
             },
             fish_fac_aureallu_mine_sweepers = {
-                name = {"Mine Sweepers"},
+                name = "Mine Sweepers",
                 text = {
                     {
                         "Use these {C:fac_fish}Fish{} for",
                         "a {C:green}#1# in #2#{} chance to",
                         "{C:attention}disable{} the {C:attention}Boss Blind",
                         "{C:inactive,s:0.8}(Currently {C:attention,s:0.8}#5#{C:inactive,s:0.8}/#6#{C:inactive,s:0.8} uses)",
-                        "{C:inactive,s:0.8}(Resets at {C:attention,s:0.8}end of round{C:inactive,s:0.8})"
+                        "{C:inactive,s:0.8}(Resets at {C:attention,s:0.8}end of round{C:inactive,s:0.8})",
+                        "{ppu_bubble:usable}"
                     },
                     {
                         "{C:green}#3# in #4#{} chance to",
@@ -399,38 +405,38 @@ return {
                 }
             },
             fish_fac_aureallu_sleeper_shark = {
-                name = {"Sleeper Shark"},
+                name = "Sleeper Shark",
                 text = {
                     {
                         "At {C:attention}end of {C:fac_fish}Fishing{},",
                         "dreams {C:attention}half{} of owned",
                         "{C:fac_sand_dollars,f:fac_sand_dollars}${} to own {C:money}sell value",
-                        "{C:inactive}(Max. {C:fac_sand_dollars,f:fac_sand_dollars}+$#1#{C:inactive})"
+                        "{C:inactive}(Maximum {C:fac_sand_dollars,f:fac_sand_dollars}+$#1#{C:inactive})"
                     },
                     {
                         "{X:fac_sand_dollars,C:white}X#2#{C:money} sell value",
                         "after {C:attention}Boss Blind",
-                        "{C:inactive}(Max. {C:fac_sand_dollars,f:fac_sand_dollars}+$#3#{C:inactive})"
+                        "{C:inactive}(Maximum {C:fac_sand_dollars,f:fac_sand_dollars}+$#3#{C:inactive})"
                     }
                 },
                 flavor = {
-                    "Shnark mimimi..", 
-                    "{s:0.8}samd dolars..{}", 
+                    "Shnark mimimi..",
+                    "{s:0.8}samd dolars..{}",
                     "Shnork mimimi..",
                 }
             },
             fish_fac_aureallu_tiger_shark = {
-                name = {"{C:attention}T{}i{C:attention}g{}e{C:attention{}r {C:attention}S{}h{C:attention}a{}r{C:attention}k"},
+                name = "{C:attention}T{}i{C:attention}g{}e{C:attention{}r {C:attention}S{}h{C:attention}a{}r{C:attention}k",
                 text = {
                     {
                         "When {C:attention}Blind{} is selected,",
-                        "{E:1,C:red}eats{} {C:fac_fish}fish{} to the right",
+                        "{E:1,C:red}eats{} {C:fac_fish}Fish{} to the right",
                         "and gains its {C:attention}weight",
                     },
                     {
                         "This Shark gives {X:mult,C:white}X#1#{} Mult",
                         "per {C:tarot}#2#kg{} it {C:attention}weighs",
-                        "(currently {X:mult,C:white}X#3#{})"
+                        "{C:inactive}(Currently {X:mult,C:white}X#3#{C:inactive} Mult)"
                     }
                 },
                 flavor = {
