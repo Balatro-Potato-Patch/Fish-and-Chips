0ff1afc2a86b0daec8e431c3e2754090fc7e4482
blamperer <137224671+blamperer@users.noreply.github.com>
2026-08-17 22:51:18 -0400

    fix timing-related crash re: fish's inferno

--- content/hayayaya/pepix/inferno.lua
+++ content/hayayaya/pepix/inferno.lua
@@ -11,7 +11,7 @@ FishAndChips.Fish({
 	},
 	atlas = "hayayaya_fih",
 	pos = { x = 1, y = 1 },
-	config = { extra = { num = 1, den = 4 } },
+	config = { extra = { num = 1, den = 4, active_bait = nil } },
 	stats = {
 		length = { min = 1, max = 1 },
 		weight = { min = 1, max = 1 },
@@ -26,8 +26,10 @@ FishAndChips.Fish({
 		}
 	end,
 	calculate = function(self, card, context)
+		if context.fac_cast_rod then
+			card.ability.extra.active_bait = G.GAME.fac_active_bait
+		end
 		if context.fac_end_fishing and not context.failed then
-			local bait = G.GAME.fac_active_bait
 			if
 				SMODS.pseudorandom_probability(
 					card,
@@ -43,7 +45,8 @@ FishAndChips.Fish({
 						if G.GAME.fac_active_bait and G.STATE ~= G.STATES.FAC_FISHING then
 							G.GAME.fac_active_bait = nil
 						end
-						FishAndChips.add_bait_to_inventory(bait, 1)
+						FishAndChips.add_bait_to_inventory(card.ability.extra.active_bait, 1)
+						card.ability.extra.active_bait = nil
 						SMODS.calculate_effect(
 							{ message = localize("ph_facyou_hayayaya_returned"), instant = true },
 							card
