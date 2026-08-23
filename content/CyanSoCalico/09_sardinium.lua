baf895b39e09c4bc8846231e6f57a8bc54dd3cae
CyanCelestial <cyancelestial@yahoo.com>
2026-08-22 21:53:40 -0700

    CyanSoCalico: Chi-Yu art

--- content/CyanSoCalico/09_sardinium.lua
+++ content/CyanSoCalico/09_sardinium.lua
@@ -1,7 +1,7 @@
 FishAndChips.Fish {
 	key = "csc_sardinium",
 	atlas = "csc_fish",
-	pos = { x = 2, y = 2 },
+	pos = { x = 2, y = 1 },
 
 	ppu_coder = { "CyanSoCalico" },
 	ppu_artist = { "CyanSoCalico" },
