2dad186a761b29f7e4bd6a8716888da477bf26eb
WilsontheWolf <git@shorty.systems>
2026-08-29 01:03:14 -0600

    fix: local list

--- content/the_s_squad/plural.lua
+++ content/the_s_squad/plural.lua
@@ -1,54 +1,26 @@
-FishAndChips.TheShitSquad.syslist = {}
-local json = require("json")
-local https = require("SMODS.https")
-local last_update_time = 0
-local initial = true
-local attempt_count = 0
+FishAndChips.TheShitSquad.syslist = {{name="elle.",colour="ff53a9"},{name="Emily"},{name="Amber",colour="83E858"},{name="Umbra",colour="B0C3F5"},{name="Sara",colour="69CEFF"},{name="Suzy",colour="C189D4"},{name="Ash",gradient={"FF5173","FFA967","FFF886","97FF61","9EEFFF","7896FE","CD9AFF","FF72CE"}},{name="Rebecca",colour="FFA837"},{name="Amy",colour="FF9BDE"},{name="Cass",colour="97FF61"},{name="???",gradient_len=3,gradient={"766A70","BD91CD"}},{name="Amity",colour="B25E6E"}}
+local function update_syslist(list)
+	for _, member in ipairs(list) do
+		member.colour = member.colour and HEX(member.colour) or nil
 
-local function update_syslist(code, body, headers)
-	--print(#body)
-	if body and #body>0 then
-		attempt_count = 0
-		local list = json.decode(body)
-		for i, member in ipairs(list) do
-			member.colour = member.colour and HEX(member.colour) or nil
-			
-			if member.gradient then
-				for i, c in ipairs(member.gradient) do
-					member.gradient[i] = c and HEX(c) or nil
-				end
-				--print("Creating effect for "..member.name)
-				member.effect = SMODS.DynaTextEffect{
-					key = "ellesys_"..member.name,
-					func = function(dynatext, index, letter)
-						local t = (G.TIMERS.REAL*3.75 + index) * 0.84 / (member.gradient_len or 1)
-						letter.colour = mix_colours(member.gradient[math.ceil(t)%#member.gradient+1], member.gradient[math.floor(t)%#member.gradient+1],t%1)
-					end
-				}
+		if member.gradient then
+			for i, c in ipairs(member.gradient) do
+				member.gradient[i] = c and HEX(c) or nil
 			end
+			member.effect = SMODS.DynaTextEffect{
+				key = "ellesys_"..member.name,
+				func = function(dynatext, index, letter)
+					local t = (G.TIMERS.REAL*3.75 + index) * 0.84 / (member.gradient_len or 1)
+					letter.colour = mix_colours(member.gradient[math.ceil(t)%#member.gradient+1], member.gradient[math.floor(t)%#member.gradient+1],t%1)
+				end
+			}
 		end
+	end
 
-		FishAndChips.TheShitSquad.syslist = list
-	else
-		attempt_count = attempt_count+1
-		if attempt_count<4 then FishAndChips.TheShitSquad.get_syslist() end
-	end
+	FishAndChips.TheShitSquad.syslist = list
 end
-function FishAndChips.TheShitSquad.get_syslist()
-	--print(https)
-	if https and https.asyncRequest then
-		--print("Making Request")
-		initial = false
-		last_update_time = os.time()
-		https.asyncRequest(
-			"https://ellestuff.dev/plurality.json",
-			update_syslist
-		)
-	end
-end
-G.E_MANAGER:add_event(Event{func=function()
-	FishAndChips.TheShitSquad.get_syslist()
-return true end})
+
+update_syslist(FishAndChips.TheShitSquad.syslist)
 
 function FishAndChips.TheShitSquad.generate_syslist_ui(list)
 	local text = {}
