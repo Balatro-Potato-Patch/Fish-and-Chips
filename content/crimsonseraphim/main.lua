b028a9dbf5b0954dce11d70e78c4e30b105ffbdf
the-Astra <djs8743@rit.edu>
2026-08-29 20:12:02 -0400

    I like to move it move it

--- content/crimsonseraphim/main.lua
+++ content/crimsonseraphim/main.lua
@@ -439,15 +439,15 @@ FishAndChips.Fish {
             trigger = "after",
             blocking = false,
             func = function()
-                if not G.GAME.fac_active_bait then
-                    FishAndChips.add_bait_to_inventory('bait_fac_normal')
-                end
                 if not G.FAC_FISH_GAME.fishing_active then
                     G.E_MANAGER:add_event(Event{
                         trigger = "after",
                         blocking = false,
                         delay = 1,
                         func = function()
+                            if not G.GAME.fac_active_bait then
+                                FishAndChips.add_bait_to_inventory('bait_fac_normal')
+                            end
                             G.FUNCS.fac_go_fish()
                             return true
                         end
