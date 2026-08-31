2c4ebbe48fc675c8df8293598742d0f14801562e
InvalidOS <35383068+InvalidOS@users.noreply.github.com>
2026-08-31 10:23:27 -0400

    make fishingslop's message use its own loc key

--- content/the_s_squad/elle.lua
+++ content/the_s_squad/elle.lua
@@ -416,7 +416,7 @@ FishAndChips.Fish {
 			if count>0 then
 				return {
 					repetitions = count,
-					message = localize("k_again_ex") .. " x" .. count
+					message = localize{ type = 'variable', key = 'k_fac_again_ex_multi', vars = {count} },
 				}
 			end
 		end
